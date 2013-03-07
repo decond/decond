2f6c1bd6ef23cab527a015f698ac821106640cb1
KM Tu <tutu522@gmail.com>
2013-03-07 22:14:51 +0900

    Update plotNoAverageCesaro-fit-ave.m and calNoAverageCesaroEC.m
    Fix a fitRange bug in plotNoAverageCesaro-fit-ave.m.
    Modifiy calNoAverageCesaroEC.m to use cumtrapz, simplifying the codes.

--- backup/plotNoAverageCesaro-fit-ave.m
+++ backup/plotNoAverageCesaro-fit-ave.m
@@ -69,9 +69,11 @@ endfor
 # md_ave(corrAve, corrIndex);
 # md_std(corrStd, corrIndex);
 md_ave = squeeze(mean(md, 1))';
-md_std = squeeze(std(md, 0, 1) ./ sqrt(numMD))';  # standard error 
+md_std = squeeze(std(md, 0, 1))';
+md_err = squeeze(std(md, 0, 1) ./ sqrt(numMD))';  # standard error 
 
-fitRange = [20, 40; 40, 60; 60, 80; 80, 100];
+fitRange = [20, 40; 40, 60; 60, 80; 80, 100]; #ps
+fitRange .*= 1000; #fs (frame)
 
 # calculate slope for each segment of md_ave
 for i = [1:size(md_ave, 2)]
@@ -79,6 +81,26 @@ for i = [1:size(md_ave, 2)]
         slope(r,i) = polyfit(timeLags(fitRange(r, 1):fitRange(r, 2)), md_ave(fitRange(r, 1):fitRange(r, 2), i), 1)(1);
     endfor
 endfor
+
+# evaluate the uncertainty in the slope of the fitting line
+# reference: Numerical Recipes Chapter 15.2 (p.656)
+for i = [1:size(md_ave, 2)]
+    for r = [1:size(fitRange, 1)]
+        rec_sig2 = 1 ./ (md_std(fitRange(r, 1):fitRange(r, 2), i) .^ 2);
+        S(r, i) = sum(rec_sig2, 1);
+        Sx(r, i) = sum(timeLags(fitRange(r, 1):fitRange(r, 2)) .* rec_sig2, 1); 
+        Sxx(r, i) = sum(timeLags(fitRange(r, 1):fitRange(r, 2)).^2 .* rec_sig2, 1); 
+        Sy(r, i) = sum(md_ave(fitRange(r, 1):fitRange(r, 2), i) .* rec_sig2, 1);
+        Syy(r, i) = sum(md_ave(fitRange(r, 1):fitRange(r, 2), i).^2 .* rec_sig2, 1);
+        Sxy(r, i) = sum(timeLags(fitRange(r, 1):fitRange(r, 2)) .* md_ave(fitRange(r, 1):fitRange(r, 2), i) .* rec_sig2, 1); 
+    endfor
+endfor
+Delta = S .* Sxx - Sx .* Sx;
+
+# output slope for double check
+slope_b = (S .* Sxy - Sx .* Sy) ./ Delta
+slopeSD = sqrt(S ./ Delta);
+# slopeSD(fitRange, corrIndex)
 
 # calculate slope for each segment of each md
 %fitRange .*= 1000;
@@ -100,9 +122,9 @@ endfor
 
 #numPlots = 1 + numIonTypes + numIonTypes*numIonTypes;
 
-# standard deviation for selected values
-stdFrameInterval = 5000;
-stdFrames = [1:stdFrameInterval:length(timeLags)]';
+# standard error for selected values
+errFrameInterval = 5000;
+errFrames = [1:errFrameInterval:length(timeLags)]';
 
 
 f1 = figure(1);
@@ -110,33 +132,25 @@ clf;
 hold on;
 
 for i = [1:size(md_ave, 2)]
-    drawFrames = stdFrames .+ (i-1)*200;
+    drawFrames = errFrames .+ (i-1)*200;
     drawFrames = drawFrames(drawFrames <= length(timeLags));
     if (i == 6)
 #        p(i).plot = plot(timeLags, md_ave(:,i), "-", "color", [0, 0.6, 0]);
-        p(i).errorbar = errorbar(timeLags(drawFrames), md_ave(drawFrames,i), md_std(drawFrames,i));
+        p(i).errorbar = errorbar(timeLags(drawFrames), md_ave(drawFrames,i), md_err(drawFrames,i));
         set(p(i).errorbar(1), "color", [0, 0.6, 0]);
     elseif (i == 7)
 #        p(i).plot = plot(timeLags, md_ave(:,i), "-", "color", [0, 0, 0]);
-        p(i).errorbar = errorbar(timeLags(drawFrames), md_ave(drawFrames,i), md_std(drawFrames,i));
+        p(i).errorbar = errorbar(timeLags(drawFrames), md_ave(drawFrames,i), md_err(drawFrames,i));
         set(p(i).errorbar(1), "color", [0, 0, 0]);
     else
         plotFormat = strcat("-", num2str(i));
         errorbarFormat = strcat("~", num2str(i));
-        p(i).errorbar = errorbar(timeLags(drawFrames), md_ave(drawFrames,i), md_std(drawFrames,i), errorbarFormat);
+        p(i).errorbar = errorbar(timeLags(drawFrames), md_ave(drawFrames,i), md_err(drawFrames,i), errorbarFormat);
 #        p(i).plot = plot(timeLags, md_ave(:,i), plotFormat);
     endif
 endfor
 
 legend("Total", "Auto Na+", "Auto Cl-", "Cross Na-Na", "Cross Na-Cl", "Cross Cl-Na", "Cross Cl-Cl", "location", "northwest");
-%p(1).plot = plot(timeLags, md_ave(:,1).ecTotalNoAverageCesaro, "-1");
-%p(2).plot = plot(timeLags, md_ave.ecAutocorrNoAverageCesaro(:,1), "-3");
-%p(3).plot = plot(timeLags, md_ave.ecAutocorrNoAverageCesaro(:,2), "-", "color", [0, 0.6, 0]);
-%p(4).plot = plot(timeLags, md_ave.ecCorrNoAverageCesaro(:,1), "-2");
-%p(5).plot = plot(timeLags, md_ave.ecCorrNoAverageCesaro(:,2), "-4");
-%p(6).plot = plot(timeLags, md_ave.ecCorrNoAverageCesaro(:,3), "-5");
-%p(7).plot = plot(timeLags, md_ave.ecCorrNoAverageCesaro(:,4), "-", "color", [0, 0, 0]);
-
 
 left = 25;
 h_space = 20;
@@ -160,7 +174,57 @@ ylabel("Non-averaged partial sum (ps*S/m)");
 axis([0,100,-600, 1000]);
 
 print(strcat('ecNoAverageCesaro-', 'ave' , '.eps'), '-deps', '-color');
-
+hold off
 %axis([0,0.1,0,1.5]);
 %print('ecNoAverageCesaro-zoom.eps', '-deps', '-color');
 
+##############################
+
+# standard deviation for selected values
+for i = [1:size(fitRange, 1)]
+    stdFrames(i) = (fitRange(i, 1) + fitRange(i, 2)) / 2;
+endfor
+
+f2 = figure(2);
+%clf;
+hold on;
+
+for i = [1:size(md_ave, 2)]
+    drawFrames = stdFrames .+ (i-1)*100;
+    if (i == 6)
+        p(i).errorbar = errorbar(timeLags(drawFrames), slope(:,i), slopeSD(:,i));
+        set(p(i).errorbar(1), "color", [0, 0.6, 0]);
+    elseif (i == 7)
+        p(i).errorbar = errorbar(timeLags(drawFrames), slope(:,i), slopeSD(:,i));
+        set(p(i).errorbar(1), "color", [0, 0, 0]);
+    else
+        plotFormat = strcat("-", num2str(i));
+        errorbarFormat = strcat("~", num2str(i));
+        p(i).errorbar = errorbar(timeLags(drawFrames), slope(:,i), slopeSD(:,i), errorbarFormat);
+    endif
+endfor
+
+legend("Total", "Auto Na+", "Auto Cl-", "Cross Na-Na", "Cross Na-Cl", "Cross Cl-Na", "Cross Cl-Cl", "location", "northwest");
+
+%left = 25;
+%h_space = 20;
+%top = 9.75;
+%v_space = 0.41;
+%for i = [1:size(md_ave, 2)]
+%    for r = [1:size(fitRange, 1)]
+%        text(left + (r-1)*h_space, top - (i-1)*v_space, num2str(slope(r, i)));
+%    endfor
+%endfor
+
+%set(p1, "linewidth", 3);
+%set(p2, "linewidth", 3);
+%set(p3, "linewidth", 3);
+%set(p4, "linewidth", 3);
+
+title(strcat("Electrical conductivity of 1m NaCl solution - md", "-ave"));
+%legend("{/Symbol D}t = 1 fs");
+xlabel("partial sum upper limit (ps)");
+ylabel("Electrical conductivity (S/m)");
+axis([0,100,-5, 10]);
+
+print(strcat('ecNoAverageCesaro-', 'ave-slope' , '.eps'), '-deps', '-color');
--- calNoAverageCesaroEC.m
+++ calNoAverageCesaroEC.m
@@ -37,46 +37,68 @@ maxLag #for showing
 
 t = [0:maxLag];
 
-function ec = integrateEC(corrData, maxLag)
-    global kB beta basicCharge ps nm volume timestep t;
-    if (length(corrData) == 1 && maxLag > 0)
-        #there is only one ion so no mutual-corr{i}{i}
-        ec = 0;
-    else
-        ec = beta * basicCharge**2 / volume * trapz(t(1:maxLag+1)', corrData(1:maxLag+1)) * timestep * nm**2 / ps;
-    endif
-endfunction
+%function ec = integrateEC(corrData, maxLag)
+%    global kB beta basicCharge ps nm volume timestep t;
+%    if (length(corrData) == 1 && maxLag > 0)
+%        #there is only one ion so no mutual-corr{i}{i}
+%        ec = 0;
+%    else
+%        ec = beta * basicCharge**2 / volume * trapz(t(1:maxLag+1)', corrData(1:maxLag+1)) * timestep * nm**2 / ps;
+%    endif
+%endfunction
 
 function index = zipIndexPair(idx1, idx2)
     global numIonTypes;
     index = (idx1 - 1) * numIonTypes + idx2;
 endfunction
 
-for T = [0:maxLag]
-    ecTotal(T+1) = 0;
-    for i = [1:numIonTypes]
-        ecAutocorr(T+1, i) = charge(i) * charge(i) * integrateEC(vAutocorr{i}, T);
-#        ecAutocorrNoAverageCesaro(T, i) = sum(ecAutocorr(1:T, i));
-        if (T > 0)
-            ecAutocorrNoAverageCesaro(T, i) = trapz([0:T]', ecAutocorr(1:T+1, i)) * timestep;
-        endif
-        ecTotal(T+1) = ecTotal(T+1) + ecAutocorr(T+1, i);
-        for j = [1:numIonTypes]
-            ecCorr(T+1,zipIndexPair(i,j)) = charge(i) * charge(j) * integrateEC(vCorr{i,j}, T);
-#            ecCorrNoAverageCesaro(T, zipIndexPair(i,j)) = sum(ecCorr(1:T, zipIndexPair(i,j)));
-            if (T > 0)
-                ecCorrNoAverageCesaro(T, zipIndexPair(i,j)) = trapz([0:T]', ecCorr(1:T+1, zipIndexPair(i,j))) * timestep;
-            endif
-            ecTotal(T+1) = ecTotal(T+1) + ecCorr(T+1, zipIndexPair(i,j));
-        endfor
-    endfor
-#    ecTotalNoAverageCesaro(T) = sum(ecTotal(1:T));
-    if (T > 0)
-        ecTotalNoAverageCesaro(T) = trapz([0:T], ecTotal(1:T+1)) * timestep;
+%for T = [0:maxLag]
+%    ecTotal(T+1) = 0;
+%    for i = [1:numIonTypes]
+%        ecAutocorr(T+1, i) = charge(i) * charge(i) * integrateEC(vAutocorr{i}, T);
+%#        ecAutocorrNoAverageCesaro(T, i) = sum(ecAutocorr(1:T, i));
+%        if (T > 0)
+%            ecAutocorrNoAverageCesaro(T, i) = trapz([0:T]', ecAutocorr(1:T+1, i)) * timestep;
+%        endif
+%        ecTotal(T+1) = ecTotal(T+1) + ecAutocorr(T+1, i);
+%        for j = [1:numIonTypes]
+%            ecCorr(T+1,zipIndexPair(i,j)) = charge(i) * charge(j) * integrateEC(vCorr{i,j}, T);
+%#            ecCorrNoAverageCesaro(T, zipIndexPair(i,j)) = sum(ecCorr(1:T, zipIndexPair(i,j)));
+%            if (T > 0)
+%                ecCorrNoAverageCesaro(T, zipIndexPair(i,j)) = trapz([0:T]', ecCorr(1:T+1, zipIndexPair(i,j))) * timestep;
+%            endif
+%            ecTotal(T+1) = ecTotal(T+1) + ecCorr(T+1, zipIndexPair(i,j));
+%        endfor
+%    endfor
+%#    ecTotalNoAverageCesaro(T) = sum(ecTotal(1:T));
+%    if (T > 0)
+%        ecTotalNoAverageCesaro(T) = trapz([0:T], ecTotal(1:T+1)) * timestep;
+%    endif
+%endfor
+%ecTotalNoAverageCesaro = ecTotalNoAverageCesaro';
+
+function ec = cumIntegrateEC(corrData, maxLag)
+    global kB beta basicCharge ps nm volume timestep t;
+    if (length(corrData) == 1 && maxLag > 0)
+        #there is only one ion so no mutual-corr{i}{i}
+        ec = 0;
+    else
+        ec = beta * basicCharge**2 / volume * cumtrapz(t(1:maxLag+1)', corrData(1:maxLag+1)) * timestep * nm**2 / ps;
     endif
+endfunction
+
+ecTotal = zeros(maxLag+1, 1); 
+for i = [1:numIonTypes]
+    ecAutocorr(:, i) = charge(i) * charge(i) * cumIntegrateEC(vAutocorr{i}, maxLag);
+    ecAutocorrNoAverageCesaro(:, i) = cumtrapz(ecAutocorr(:, i)) * timestep;
+    ecTotal .+= ecAutocorr(:, i);
+    for j = [1:numIonTypes]
+        ecCorr(:, zipIndexPair(i,j)) = charge(i) * charge(j) * cumIntegrateEC(vCorr{i,j}, maxLag);
+        ecCorrNoAverageCesaro(:, zipIndexPair(i,j)) = cumtrapz(ecCorr(:, zipIndexPair(i,j))) * timestep;
+        ecTotal .+= ecCorr(:, zipIndexPair(i,j));
+    endfor
 endfor
-ecTotalNoAverageCesaro = ecTotalNoAverageCesaro';
+ecTotalNoAverageCesaro = cumtrapz(ecTotal) * timestep;
 
-timeLags = [1:maxLag]' * timestep;
-save(strcat(baseFilename, ".ecNoAverageCesaro"), "timestep", "timeLags", "ecTotalNoAverageCesaro", "ecAutocorrNoAverageCesaro", "ecCorrNoAverageCesaro");
-
+timeLags = [0:maxLag]' * timestep;
+save(strcat(baseFilename, ".ecNoAverageCesaro-cum"), "timestep", "timeLags", "ecTotalNoAverageCesaro", "ecAutocorrNoAverageCesaro", "ecCorrNoAverageCesaro");
