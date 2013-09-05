4df2c414dc30ac39a41f81e58b3b5347d8813313
KM Tu <tutu522@gmail.com>
2013-09-06 01:57:44 +0900

    Backup commit: analyze.sh

--- backup/analyze.sh
+++ backup/analyze.sh
@@ -20,11 +20,13 @@ systemVolume=$(tail -n 1 frame$idx.gro | echo "$(awk '{print $1}')^3" | bc)
 echo "box length = $boxLength"
 echo "system volume = $systemVolume"
 
-GMDIR=/home/kmtu/local/gromacs-4.5.5/bin
+#GMDIR=/home/kmtu/local/gromacs-4.5.5/bin
+GMDIR=/home/kmtu/local/gromacs-4.6.3/bin
 #NDXDIR=/home/kmtu/elec_cond/NaCl/s36-w1000/NVE
 ECDIR=/home/kmtu/elec_cond/econd
+TRJCONV=$GMDIR/trjconv_mpi_d
 
-mdData=md.trr
+mdData=md-na-cl.trr
 naBase=md-na-skip$trajInterval
 clBase=md-cl-skip$trajInterval
 naData=$naBase.trr
@@ -36,18 +38,20 @@ clVBin=${clBase}_d_vdata.binary
 
 maxlag=1000000
 dataMaxlag=$((maxlag/trajInterval))
-outName=lag$maxlag-skip$trajInterval
+#outName=lag$maxlag-skip$trajInterval
+outName=E6E5-skip$trajInterval
 
 analyzeMaxlag=$((100000/trajInterval))
 
-#$GMDIR/trjconv_d -f $mdData -o $naData -n ../na.ndx -skip $trajInterval &&\
-#$GMDIR/trjconv_d -f $mdData -o $clData -n ../cl.ndx -skip $trajInterval 
+#$TRJCONV -f md-na-cl.g96 -o $mdData
+#$TRJCONV -f $mdData -o $naData -n ../na-pure.ndx -skip $trajInterval &&\
+#$TRJCONV -f $mdData -o $clData -n ../cl-pure.ndx -skip $trajInterval 
 
 #$ECDIR/convertData.m $naData $naBase double &&\
 #$ECDIR/convertData.m $clData $clBase double 
 #$ECDIR/calCorr.m $outName $dataMaxlag double $naVBin 1 $clVBin -1
 
-$ECDIR/spatialDecompose.m $outName $dataMaxlag 0.1 $boxLength double $naXBin $naVBin 1 $clXBin $clVBin -1
+#$ECDIR/spatialDecompose.m $outName $dataMaxlag 0.1 $boxLength double $naXBin $naVBin 1 $clXBin $clVBin -1
 #$ECDIR/calDC.m $outName.vCorr $analyzeMaxlag &&\
 #$ECDIR/calEC.m $outName.vCorr $analyzeMaxlag $systemVolume &&\
 #mv $outName.dc ${outName}-int_$analyzeMaxlag.dc &&\
@@ -56,8 +60,8 @@ $ECDIR/spatialDecompose.m $outName $dataMaxlag 0.1 $boxLength double $naXBin $na
 
 #for d in 15 20 25 30 35 40 
 #do
-#    d=1
-#    $ECDIR/calNoAverageCesaroEC.m $outName.vCorr $analyzeMaxlag $systemVolume $d
+    d=1
+    $ECDIR/calNoAverageCesaroEC.m $outName.vCorr $analyzeMaxlag $systemVolume $d
 #done
 
 cd ..
