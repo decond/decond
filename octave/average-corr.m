3660ee714adc2ce826305639d653fecc61b756b8
KM Tu <tutu522@gmail.com>
2013-12-06 00:53:35 +0900

    Update average-corr.m to take the average of oneTwoDecompose

--- octave/average-corr.m
+++ octave/average-corr.m
@@ -5,7 +5,7 @@ format long
 global numIonTypes
 
 if (nargin() < 2)
-    error(cstrcat("Usage: $average-vCorr.m <data.vCorr> <numMD>")); 
+    error(cstrcat("Usage: $average-corr.m <data.corr> <numMD>")); 
 endif
 
 dataFilename = argv(){1}
@@ -14,78 +14,45 @@ numMD = str2num(argv(){2})
 %extnamePos = rindex(filename, "."); #locate the position of the extension name
 %baseFilename = dataFilename(1:extnamePos-1)
 
-
-for n = [1:numMD]
-    dataPath{n} = strcat("./md", num2str(n-1), "/", dataFilename);
-    md(n) = load(dataPath{n});
-endfor
-
-timestep = md(1).timestep;
-charge = md(1).charge;
-numAtoms = md(1).numAtoms;
-timeLags = md(1).timeLags;
+load(strcat("./md0/", dataFilename));
+clear("autoCorr");
+clear("crossCorr");
 numIonTypes = length(charge)
 
 function index = zipIndexPair(idx1, idx2)
+    global numIonTypes;
+    index = (idx1 - 1) * numIonTypes + idx2;
+endfunction
+
+function index = zipIndexPair2(idx1, idx2)
     global numIonTypes;
 %    index = (idx1 - 1) * numIonTypes + idx2;
     # accept only the "upper-half" index pair, because cross-correlation should 
     # be the same for (i,j) and (j,i)
     if (idx1 > idx2)
-      error("Error - zipIndexPair: idx1 > idx2");
+      error("Error in zipIndexPair2: idx1 > idx2");
     else
       index = (idx1 - 1) * numIonTypes + idx2 - (idx1-1);
     endif
 endfunction
 
-vAutocorr_ave = zeros(length(timeLags), numIonTypes);
-vCorr_ave = zeros(length(timeLags), numIonTypes*(numIonTypes+1)/2);
-%vAutocorr = cell(size(md(1).vAutocorr));
-%vCorr = cell(size(md(2).vCorr));
-%vAutocorr(:) = 0;
-%vCorr(:) = 0;
+numIonTypePairs = (numIonTypes*(numIonTypes+1)) / 2;
 
-% sum
+# loading data 
 for n = [1:numMD]
-    for i = [1:numIonTypes]
-        vAutocorr_ave(:,i) += md(n).vAutocorr{i};
-        for j = [i:numIonTypes]
-            idx = zipIndexPair(i,j);
-            vCorr_ave(:,idx) .+= (md(n).vCorr{i,j} .+ md(n).vCorr{j,i}) ./ 2;
-        endfor
-    endfor
+    data_tmp = load(strcat("./md", num2str(n-1), "/", dataFilename));
+    autoCorrN(:, :, n) = data_tmp.autoCorr;
+    crossCorrN(:, :, n) = data_tmp.crossCorr;
 endfor
+clear("data_tmp");
 
-% average    
-vAutocorr_ave /= numMD;
-vCorr_ave /= numMD;
-%for i = [1:length(vAutocorr)]
-%    vAutocorr{i} = vAutocorr{i} / numMD;
-%    for j = [1:length(vAutocorr)]
-%        vCorr{i,j} = vCorr{i,j} / numMD;
-%    endfor
-%endfor
+autoCorr.ave = mean(autoCorrN, 3);
+crossCorr.ave = mean(crossCorrN, 3);
+autoCorr.std= std(autoCorrN, 0, 3);
+crossCorr.std = std(crossCorrN, 0, 3);
+autoCorr.err = autoCorr.std / sqrt(numMD);
+crossCorr.err = crossCorr.std / sqrt(numMD);
 
-vAutocorr_std = zeros(size(vAutocorr_ave));
-vCorr_std = zeros(size(vCorr_ave));
-%std
-for n = [1:numMD]
-    for i = [1:numIonTypes]
-        vAutocorr_std(:,i) += (md(n).vAutocorr{i} .- vAutocorr_ave(:, i)).**2;
-        for j = [i:numIonTypes]
-            idx = zipIndexPair(i,j);
-            vCorr_std(:,idx) += ((md(n).vCorr{i,j} + md(n).vCorr{j,i})/2 .- vCorr_ave(:,idx)).**2;
-        endfor
-    endfor
-endfor
-vAutocorr_std /= (numMD - 1);
-vCorr_std /= (numMD - 1);
-vAutocorr_std = sqrt(vAutocorr_std);
-vCorr_std = sqrt(vCorr_std);
-
-vAutocorr_err = vAutocorr_std ./ sqrt(numMD);
-vCorr_err = vCorr_std ./ sqrt(numMD);
-
-save(strcat(dataFilename, '-ave', num2str(numMD)), "timestep", "charge", "numAtoms", "timeLags",\
-     "vAutocorr_ave", "vCorr_ave", "vAutocorr_err", "vCorr_err" );
+save(strcat(dataFilename, '-ave', num2str(numMD)), "timestep", "charge", "numAtom", "timeLags",\
+     "autoCorr", "crossCorr");
 
