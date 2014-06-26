3d9ed5d8a62bbb3ded2fa35e872e12c83c7ac0bf
KM Tu <tutu522@gmail.com>
2014-06-27 01:50:39 +0900

    top.f90 is finished
    atom mass and charge in top files can be read if they are
    written in the [ atom ] records under [ moleculetype ]
    
    Reading [ atomtype ] is not implemented

--- fortran/src/top.f90
+++ fortran/src/top.f90
@@ -1,17 +1,17 @@
 module top
-  use utility, only : handle, newunit
+  use utility, only : handle, newunit, LINE_LEN, count_record_in_string
   implicit none
 
   private
-  public :: open_top, close_top, read_top, topology
+  public :: open_top, close_top, read_top, system
   
-  integer, parameter :: LINE_LEN = 128
   character(len=*), parameter :: LINE_LEN_STR = "128"
   character(len=*), parameter :: COMMENT_CHAR= ";"
   
   character(len=LINE_LEN) :: current_directive = ''
 
   type atom
+    character(len=LINE_LEN) :: type
     real(8) :: mass
     real(8) :: charge
   end type atom
@@ -19,22 +19,21 @@ module top
   type molecule
     character(len=LINE_LEN) :: name
     integer :: num  ! number of molecules of the same type
-    integer :: num_atom  ! number of atoms per molecule
     type(atom), dimension(:), allocatable :: atom
   end type molecule
 
-  type topology
-    integer :: num_moltype
+  type system
     type(molecule), dimension(:), allocatable :: mol
-  end type topology
+  end type system
   
 contains
-  subroutine open_top(htop, filename)
+  type(handle) function open_top(filename)
     implicit none
-    type(handle), intent(inout) :: htop
     character(len=*), intent(in) :: filename
-    open(unit=newunit(htop%iohandle), file=filename, status='old', action="READ", form="FORMATTED")
-  end subroutine open_top
+  
+    open_top%filename = filename
+    open(unit=newunit(open_top%iohandle), file=filename, status='old', action="READ", form="FORMATTED")
+  end function open_top
 
   subroutine close_top(htop)
     implicit none
@@ -45,24 +44,181 @@ contains
   subroutine read_top(htop, tp)
     implicit none
     type(handle), intent(in) :: htop
-    type(topology), intent(out) :: tp
-    integer :: stat
-    character(len=LINE_LEN) :: line
-    integer :: num_moltype
+    type(system), intent(out) :: tp
+    integer :: i
 
-    num_moltype = count_moltype(htop)
-    write(*,*) "num_moltype=", num_moltype
+    call read_top_system(htop, tp)
+    call read_top_molecule(htop, tp%mol)
 
-    allocate(tp%mol(num_moltype))
-
-    stat = 0
-    do while(.true.)
-      call read_line(htop, line, status=stat)
-      if (stat < 0) then
-        exit  ! EOF
-      end if
+    write(*,*) "[ molecules ]"
+    do i = 1, size(tp%mol)
+      write(*, *) adjustl(trim(tp%mol(i)%name)), tp%mol(i)%num
     end do
   end subroutine read_top
+
+  subroutine read_top_system(htop, tp)
+    implicit none
+    type(handle), intent(in) :: htop
+    type(system), intent(out) :: tp
+    integer :: stat
+    character(len=LINE_LEN) :: line
+    integer :: num_moltype, i
+
+    num_moltype = count_moltype(htop)
+    allocate(tp%mol(num_moltype))
+
+    ! read name and number of molecules
+    call read_to_next_directive(htop, 'molecules', status=stat)
+    if (stat < 0) then
+      write(*,*) "Error: no [ molecules ] in the top file"
+      call exit(1)
+    end if
+
+    do i = 1, num_moltype
+      call read_line(htop, line, status=stat)
+      if (stat /= 0) then
+        write(*,*) "Error reading name and number of molecules"
+        call exit(1)
+      end if
+      read(line, *) tp%mol(i)%name, tp%mol(i)%num
+    end do
+    rewind(htop%iohandle)
+  end subroutine read_top_system
+
+  subroutine read_top_molecule(htop, mol)
+    implicit none
+    type(handle), intent(in) :: htop
+    type(molecule), dimension(:), intent(inout) :: mol
+    integer :: stat
+    character(len=LINE_LEN) :: line, dum_s
+    integer :: i, j, num_atom, num_rec, dum_i
+
+    do i = 1, size(mol)
+      num_atom = count_molatom(htop, mol(i))
+      if (num_atom == 0) then
+        write(*,*) "Error: no atom records for ", adjustl(trim(mol(i)%name))
+        call exit(1)
+      end if
+      allocate(mol(i)%atom(num_atom))
+      call read_to_moltype(htop, mol(i), stat)
+      call read_line(htop, line, status=stat) ! skip [ atom ] line
+write(*,*) "[ moleculetype ]"
+write(*,*) trim(mol(i)%name)
+write(*,*) "[ atom ]"
+write(*,*) "type  mass  charge"
+      do j = 1, num_atom
+        call read_line(htop, line, status=stat)
+        num_rec = count_record_in_string(line)
+        select case (num_rec)
+!          case (7)
+!            read(line, *) dum_i, mol(i)%atom(j)%type, dum_i, dum_s, dum_s, dum_i, &
+!                          mol(i)%atom(j)%charge
+          case (8)
+            read(line, *) dum_i, mol(i)%atom(j)%type, dum_i, dum_s, dum_s, dum_i, &
+                          mol(i)%atom(j)%charge, mol(i)%atom(j)%mass
+          case default
+            write(*,*) "Error: I cannot deal with num_rec=", num_rec, " for molecule ", mol(i)%name
+            call exit(1)
+        end select
+write(*,*) trim(mol(i)%atom(j)%type), " ", mol(i)%atom(j)%mass, mol(i)%atom(j)%charge
+      end do
+write(*,*)
+    end do
+    rewind(htop%iohandle)
+  end subroutine read_top_molecule
+
+  integer function count_molatom(htop, mol)
+    implicit none
+    type(handle), intent(in) :: htop
+    type(molecule), intent(inout) :: mol
+    integer :: stat
+    character(len=LINE_LEN) :: line
+
+    count_molatom = 0
+    stat = 0
+
+    call read_to_moltype(htop, mol, stat)
+    if (stat < 0) then
+      return  ! EOF
+    end if
+    
+    call read_next_directive(htop, stat)
+    if (stat < 0) then
+      return  ! EOF
+    else if (current_directive /= 'atoms') then
+      write(*,*) "Error: no [ atoms ] found after [ moleculetype ] ", trim(mol%name)
+      write(*,*) "Note: [ atoms ] must be the next directive of [ moleculetype ]"
+      call exit(1)
+    end if
+    do while(.true.)
+      call read_line(htop, line, status=stat)
+      if (current_directive == 'atoms' .and. stat == 0) then
+        count_molatom = count_molatom + 1
+      else
+        rewind(htop%iohandle)
+        return
+      end if
+    end do
+  end function count_molatom
+
+  subroutine read_to_moltype(htop, mol, status)
+    implicit none
+    type(handle), intent(in) :: htop
+    type(molecule), intent(in) :: mol
+    integer, intent(out) :: status
+    character(len=LINE_LEN) :: line, name
+    
+    do while(.true.)
+      call read_to_next_directive(htop, 'moleculetype', status)
+      if (status < 0) then
+        return  ! EOF
+      else if (status == 1) then
+        call read_line(htop, line, status=status)
+        if (status < 0) then
+          return ! EOF
+        else if (status == 0) then
+          read(line, *) name
+          if (name == mol%name) then
+            return 
+          end if
+        end if
+      end if
+    end do
+  end subroutine read_to_moltype
+  
+  subroutine read_to_next_directive(htop, directive, status)
+    implicit none
+    type(handle), intent(in) :: htop
+    character(len=*), intent(in) :: directive
+    integer, intent(out) :: status
+    character(len=LINE_LEN) :: line
+    do while(.true.)
+      call read_line(htop, line, status=status)
+      if (status < 0) then
+        return ! EOF
+      end if
+      if (current_directive == directive .and. status == 1) then
+        ! status == 1 means successfully read to a new directive
+        return
+      end if
+    end do
+  end subroutine read_to_next_directive
+
+  subroutine read_next_directive(htop, status)
+    implicit none
+    type(handle), intent(in) :: htop
+    integer, intent(out) :: status
+    character(len=LINE_LEN) :: line
+    do while(.true.)
+      call read_line(htop, line, status=status)
+      if (status < 0) then
+        return ! EOF
+      end if
+      if (status == 1) then
+        return  ! next directive returned as current_directive
+      end if
+    end do
+  end subroutine read_next_directive
 
   integer function count_moltype(htop)
     implicit none
@@ -138,17 +294,18 @@ contains
   end subroutine remove_comment
 end module top
 
-program test
-  use utility, only : handle
-  use top, only: open_top, close_top, read_top, topology
-  implicit none
-  integer, parameter :: LINE_LEN = 128
-  character(len=LINE_LEN) :: top_filename
-  type(handle) :: htop
-  type(topology) :: tp
-  call get_command_argument(1, top_filename)
-  call open_top(htop, top_filename)
-  call read_top(htop, tp)
-  call close_top(htop)
-end program test
+!program test
+!  use utility, only : handle
+!  use top, only: open_top, close_top, read_top, system
+!  implicit none
+!  integer, parameter :: LINE_LEN = 128
+!  character(len=LINE_LEN) :: top_filename
+!  type(handle) :: htop
+!  type(system) :: tp
+!
+!  call get_command_argument(1, top_filename)
+!  htop = open_top(top_filename)
+!  call read_top(htop, tp)
+!  call close_top(htop)
+!end program test
 
