aa665a313aefa753271084969eda1d9e5bb84b29
KM Tu <tutu522@gmail.com>
2014-06-30 23:21:38 +0900

    Makes top.f90 read information from [ atomtypes ]
    and adds several improvements to accomodate the needs of
    oneTwoDecompose.f90

--- fortran/src/top.f90
+++ fortran/src/top.f90
@@ -6,7 +6,7 @@ module top
   public :: open_top, close_top, read_top, system
   
   character(len=*), parameter :: LINE_LEN_STR = "128"
-  character(len=*), parameter :: COMMENT_CHAR= ";"
+  character(len=1), dimension(2), parameter :: COMMENT_CHAR = [";", "#"]
   
   character(len=LINE_LEN) :: current_directive = ''
 
@@ -17,7 +17,7 @@ module top
   end type atom
 
   type molecule
-    character(len=LINE_LEN) :: name
+    character(len=LINE_LEN) :: type
     integer :: num  ! number of molecules of the same type
     type(atom), dimension(:), allocatable :: atom
   end type molecule
@@ -41,31 +41,49 @@ contains
     close(htop%iohandle)
   end subroutine close_top
 
-  subroutine read_top(htop, tp)
+  subroutine read_top(htop, sys)
     implicit none
     type(handle), intent(in) :: htop
-    type(system), intent(out) :: tp
-    integer :: i
+    type(system), intent(inout) :: sys
+    type(system) :: sys_top
+    integer :: i, j
 
-    call read_top_system(htop, tp)
-    call read_top_molecule(htop, tp%mol)
+    call read_top_system(htop, sys_top)
+    call read_top_molecule(htop, sys_top%mol)
+
+    do i = 1, size(sys%mol)
+      do j = 1, size(sys_top%mol)
+        if (sys%mol(i)%type == sys_top%mol(j)%type) then
+          sys%mol(i) = sys_top%mol(j)
+        end if
+      end do
+    end do
 
     write(*,*) "[ molecules ]"
-    do i = 1, size(tp%mol)
-      write(*, *) adjustl(trim(tp%mol(i)%name)), tp%mol(i)%num
+    do i = 1, size(sys%mol)
+      write(*, *) adjustl(trim(sys%mol(i)%type)), sys%mol(i)%num
+    end do
+    do i = 1, size(sys%mol)
+      write(*,*) "[ moleculetype ]"
+      write(*, *) adjustl(trim(sys%mol(i)%type))
+      write(*,*) "[ atoms ]"
+      write(*,*) "type  mass  charge"
+      do j = 1, size(sys%mol(i)%atom)
+        write(*,*) trim(sys%mol(i)%atom(j)%type), " ", sys%mol(i)%atom(j)%mass, sys%mol(i)%atom(j)%charge
+      end do
     end do
   end subroutine read_top
 
-  subroutine read_top_system(htop, tp)
+  subroutine read_top_system(htop, sys)
     implicit none
     type(handle), intent(in) :: htop
-    type(system), intent(out) :: tp
+    type(system), intent(out) :: sys
     integer :: stat
     character(len=LINE_LEN) :: line
     integer :: num_moltype, i
 
     num_moltype = count_moltype(htop)
-    allocate(tp%mol(num_moltype))
+    allocate(sys%mol(num_moltype))
 
     ! read name and number of molecules
     call read_to_next_directive(htop, 'molecules', status=stat)
@@ -80,7 +98,7 @@ contains
         write(*,*) "Error reading name and number of molecules"
         call exit(1)
       end if
-      read(line, *) tp%mol(i)%name, tp%mol(i)%num
+      read(line, *) sys%mol(i)%type, sys%mol(i)%num
     end do
     rewind(htop%iohandle)
   end subroutine read_top_system
@@ -96,36 +114,106 @@ contains
     do i = 1, size(mol)
       num_atom = count_molatom(htop, mol(i))
       if (num_atom == 0) then
-        write(*,*) "Error: no atom records for ", adjustl(trim(mol(i)%name))
+        write(*,*) "Error: no atom records for ", adjustl(trim(mol(i)%type))
         call exit(1)
       end if
       allocate(mol(i)%atom(num_atom))
+
+      ! read atomtypes from moleculetype
       call read_to_moltype(htop, mol(i), stat)
       call read_line(htop, line, status=stat) ! skip [ atom ] line
-write(*,*) "[ moleculetype ]"
-write(*,*) trim(mol(i)%name)
-write(*,*) "[ atom ]"
-write(*,*) "type  mass  charge"
       do j = 1, num_atom
+        call read_line(htop, line, status=stat)
+        read(line, *) dum_i, mol(i)%atom(j)%type
+      end do
+    end do
+
+    ! read data from [ atomtypes ] first
+    do i = 1, size(mol)
+      do j = 1, size(mol(i)%atom)
+        call read_top_atype(htop, mol(i)%atom(j))
+      end do
+    end do
+
+    ! read data from [ moleculetype ] -> [ atoms ]
+    do i = 1, size(mol)
+      call read_to_moltype(htop, mol(i), stat)
+      call read_line(htop, line, status=stat) ! skip [ atoms ] line
+      do j = 1, size(mol(i)%atom)
         call read_line(htop, line, status=stat)
         num_rec = count_record_in_string(line)
         select case (num_rec)
-!          case (7)
-!            read(line, *) dum_i, mol(i)%atom(j)%type, dum_i, dum_s, dum_s, dum_i, &
-!                          mol(i)%atom(j)%charge
+          case (7)
+            read(line, *) dum_i, mol(i)%atom(j)%type, dum_i, dum_s, dum_s, dum_i, &
+                          mol(i)%atom(j)%charge
           case (8)
             read(line, *) dum_i, mol(i)%atom(j)%type, dum_i, dum_s, dum_s, dum_i, &
                           mol(i)%atom(j)%charge, mol(i)%atom(j)%mass
           case default
-            write(*,*) "Error: I cannot deal with num_rec=", num_rec, " for molecule ", mol(i)%name
+            write(*,*) "Error: I cannot deal with num_rec=", num_rec, " for molecule ", mol(i)%type
             call exit(1)
         end select
-write(*,*) trim(mol(i)%atom(j)%type), " ", mol(i)%atom(j)%mass, mol(i)%atom(j)%charge
       end do
-write(*,*)
     end do
     rewind(htop%iohandle)
   end subroutine read_top_molecule
+
+  subroutine read_top_atype(htop, at)
+    implicit none
+    type(handle), intent(in) :: htop
+    type(atom), intent(inout) :: at
+    integer :: stat
+    character(len=LINE_LEN) :: line, atype, dum_s
+    integer :: i, j, num_atom, num_rec, dum_i
+    real(8) :: dum_r, mass, charge
+    logical :: is_read
+
+    is_read = .false.
+    rewind(htop%iohandle)
+    do while(.true.)
+      call read_to_next_directive(htop, "atomtypes", stat)
+      if (stat < 0) exit  ! EOF
+      do while(.true.)
+        call read_line(htop, line, status=stat)
+        if (stat < 0) then
+          exit  ! EOF
+        else if (stat == 1) then  ! new directive is met
+          if (current_directive == "atomtypes") then
+            cycle
+          else
+            exit
+          end if
+        end if
+        num_rec = count_record_in_string(line)
+        select case (num_rec)
+          case (6)
+            read(line, *, iostat=stat) atype, mass, charge, dum_s, dum_r, dum_r
+          case (7)
+            read(line, *, iostat=stat) atype, dum_i, mass, charge, dum_s, dum_r, dum_r
+          case default
+            write(*,*) "Error: I cannot deal with num_rec=", num_rec, " for atomtype ", trim(at%type)
+            write(*,*) trim(line)
+            call exit(1)
+        end select
+        if (stat /= 0) then
+          write(*,*) "Error: something went wrong when reading atomtype ", trim(at%type)
+          write(*,*) trim(line)
+          call exit(1)
+        end if 
+        if (atype == at%type) then
+          is_read = .true.
+          at%mass = mass
+          at%charge = charge
+        end if
+      end do
+    end do
+
+    if (.not. is_read) then
+      write(*,*) "Error: no [ atomtypes ] for ", trim(at%type), " is read"
+      call exit(1)
+    end if
+    rewind(htop%iohandle)
+  end subroutine read_top_atype
 
   integer function count_molatom(htop, mol)
     implicit none
@@ -134,6 +222,7 @@ write(*,*)
     integer :: stat
     character(len=LINE_LEN) :: line
 
+    rewind(htop%iohandle)
     count_molatom = 0
     stat = 0
 
@@ -146,7 +235,7 @@ write(*,*)
     if (stat < 0) then
       return  ! EOF
     else if (current_directive /= 'atoms') then
-      write(*,*) "Error: no [ atoms ] found after [ moleculetype ] ", trim(mol%name)
+      write(*,*) "Error: no [ atoms ] found after [ moleculetype ] ", trim(mol%type)
       write(*,*) "Note: [ atoms ] must be the next directive of [ moleculetype ]"
       call exit(1)
     end if
@@ -168,6 +257,7 @@ write(*,*)
     integer, intent(out) :: status
     character(len=LINE_LEN) :: line, name
     
+    rewind(htop%iohandle)
     do while(.true.)
       call read_to_next_directive(htop, 'moleculetype', status)
       if (status < 0) then
@@ -178,12 +268,13 @@ write(*,*)
           return ! EOF
         else if (status == 0) then
           read(line, *) name
-          if (name == mol%name) then
+          if (name == mol%type) then
             return 
           end if
         end if
       end if
     end do
+    rewind(htop%iohandle)
   end subroutine read_to_moltype
   
   subroutine read_to_next_directive(htop, directive, status)
@@ -285,12 +376,14 @@ write(*,*)
   subroutine remove_comment(line)
     implicit none
     character(len=*), intent(inout) :: line
-    integer :: idx
+    integer :: idx, i
 
-    idx = index(line, COMMENT_CHAR)
-    if (idx > 0) then
-      line = line(:idx - 1)
-    end if
+    do i = 1, size(COMMENT_CHAR)
+      idx = index(line, COMMENT_CHAR(i))
+      if (idx > 0) then
+        line = line(:idx - 1)
+      end if
+    end do
   end subroutine remove_comment
 end module top
 
@@ -301,11 +394,11 @@ end module top
 !  integer, parameter :: LINE_LEN = 128
 !  character(len=LINE_LEN) :: top_filename
 !  type(handle) :: htop
-!  type(system) :: tp
+!  type(system) :: sys
 !
 !  call get_command_argument(1, top_filename)
 !  htop = open_top(top_filename)
-!  call read_top(htop, tp)
+!  call read_top(htop, sys)
 !  call close_top(htop)
 !end program test
 
