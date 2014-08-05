cc259e83b6172713fbdad3ba5208cd97fd972486
KM Tu <tutu522@gmail.com>
2014-08-06 04:15:20 +0900

    plotSdD.py now plots RDF, D_I/D_IL, and sig_I at once!
    There may be some problems in the sig_I figure.
    The summed value seems to be twice larger than the total EC.

--- python/plotSdD.py
+++ python/plotSdD.py
@@ -3,6 +3,7 @@ import argparse
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import accumulate
 
 parser = argparse.ArgumentParser(description="Plot and examine the results from fitSdDCesaro.py and fitNDCesaro.py")
 parser.add_argument('-ND', '--NDCesaroFit', help="fitted ND results data file <NDCesaro.fit.h5>")
@@ -20,6 +21,17 @@ class Const:
   basicCharge = 1.60217646E-19 #(Coulomb)
   ps = 1.0E-12 #(s)
   nm = 1.0E-9 #(m)
+  nm2AA = 10
+
+def zipIndexPair2(idx_r, idx_c, size):
+  """
+  Returns the single index of a upper-half matrix based the row index and column index
+
+  accepts only the "upper-half" index pair, because cross-correlation should
+  be the same for (i,j) and (j,i)
+  """
+  assert(idx_r <= idx_c)
+  return idx_r * size - ([0]+list(accumulate(range(4))))[idx_r] + idx_c - idx_r
 
 def loadDictFromH5(h5g):
   dict = {}
@@ -27,13 +39,6 @@ def loadDictFromH5(h5g):
     dict[k] = v[...]
   h5g.visititems(func)
   return dict
-
-if (args.NDCesaroFit != None):
-  with h5py.File(args.NDCesaroFit, 'r') as fND:
-    zz = fND['zz'][...]
-    ND = loadDictFromH5(fND['ND'])
-    volumeND = fND['volume'][...]
-    Const.ND2ecSI = Const.beta * Const.basicCharge**2 / (volumeND*(Const.nm**3)) * Const.nm**2 / Const.ps
 
 with h5py.File(args.sdDCesaroFit, 'r') as fSdD:
   charge = fSdD.attrs['charge'][...]
@@ -51,11 +56,22 @@ with h5py.File(args.sdDCesaroFit, 'r') as fSdD:
   sdD_err = loadDictFromH5(fSdD['sdD_err'])
   Const.sdD2ecSI = Const.beta * Const.basicCharge**2 / (volumeSdD*(Const.nm**3)) * Const.nm**2 / Const.ps
 
-density = numMol / volumeSdD
+if (args.NDCesaroFit != None):
+  with h5py.File(args.NDCesaroFit, 'r') as fND:
+    zz = fND['zz'][...]
+    ND = loadDictFromH5(fND['ND'])
+    volumeND = fND['volume'][...]
+    Const.ND2ecSI = Const.beta * Const.basicCharge**2 / (volumeND*(Const.nm**3)) * Const.nm**2 / Const.ps
 
+  DI = {}
+  sigAutoI = {}
+  for fit in ND:
+    DI[fit] = ND[fit][:numMol.size] / numMol * Const.nm2AA**2  # AA^2 / ps
+    sigAutoI[fit] = ND[fit][:numMol.size] * zz[:numMol.size] * Const.ND2ecSI
+
+density = numMol / volumeSdD
 dr = rBins[1] - rBins[0]
 dv = 4 * np.pi * rBins**2 * dr
-
 vol = volumeSdD
 rho_Vdv = rho2
 rho_V = rho_Vdv / dv
@@ -65,9 +81,56 @@ g = rho / np.array([d1 * d2 for (e1, d1) in enumerate(density)
                             for (e2, d2) in enumerate(density) if e2 >= e1]
                   )[:, np.newaxis]
 
-p = []
-for i, rdf in enumerate(g):
-    p.append(plt.plot(rBins, rdf, label='{}'.format(i)))
+
+rBins *= Const.nm2AA
+figs = []
+numPlots = 3 if (args.NDCesaroFit != None) else 2
+
+sigIL = {}
+sigI = {}
+for fit in sdD:
+  sigIL[fit] = rho_dv / Const.nm2AA**3 * sdD[fit] * zzCross[:, np.newaxis] * \
+             Const.beta * Const.basicCharge**2 
+  sigI[fit] = sigAutoI[fit][:, np.newaxis] * np.ones_like(rBins)
+  for r in range(numMol.size):
+    for c in range(r, numMol.size):
+      sigI[fit][r] += sigIL[fit][zipIndexPair2(r,c, numMol.size)]
+      if (r != c):
+        sigI[fit][c] += sigIL[fit][zipIndexPair2(r,c, numMol.size)]
+
+threshold = 0.1
+for fitKey in sorted(sdD, key=lambda x:x.split(sep='-')[0]):
+  fig, axs = plt.subplots(numPlots, 1, sharex=True)
+  figs.append(fig)
+
+  sdD[fitKey] *= Const.nm2AA**2
+
+  for i, rdf in enumerate(g):
+    axs[0].plot(rBins, rdf, label='{}'.format(i))
+  axs[0].legend()
+  axs[0].set_title("Fit {} ps".format(fitKey))
+  axs[0].set_ylabel(r"$g_{IL}(r)$")
+
+  i = -1
+  if (args.NDCesaroFit != None):
+    for i, D in enumerate(DI[fitKey]):
+      axs[1].plot(rBins, np.ones_like(rBins)*D, label='{}'.format(i), linestyle='--')
+      axs[1].legend()
+    axs[1].set_ylabel(r"$D_I, D_{IL}(r)$  ($\AA^2$ ps$^{-1}$)")
+  else:
+    axs[1].set_ylabel(r"$D_{IL}(r)$  ($\AA^2$ ps$^{-1}$)")
+  for j, D in enumerate(sdD[fitKey]):
+    axs[1].plot(rBins, np.ma.masked_where(g[j] < threshold, D), label='{}'.format(i+j+1))
+    axs[1].legend()
+    axs[1].set_title("threshold {}".format(threshold))
+
+  if (args.NDCesaroFit != None):
+    for i, sig in enumerate(sigI[fitKey]):
+      axs[2].plot(rBins, sig, label='{}'.format(i))
+      axs[2].legend()
+    axs[2].set_ylabel(r"$\sigma_I(\lambda)$  (S m$^{-1}$)")
+
+  plt.xlim(xmax=rBins[rBins.size / 2])
 
 plt.ion()
 plt.show()
