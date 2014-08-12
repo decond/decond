ff2f48863312d42e06e15bbbc850b0784a1a8654
KM Tu <tutu522@gmail.com>
2014-08-12 23:49:04 +0900

    Backup: adjusts the cbar for a specific IL case

--- python/plotSdCorr.py
+++ python/plotSdCorr.py
@@ -3,6 +3,8 @@ import argparse
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from  matplotlib import cm
 from itertools import accumulate
 
 parser = argparse.ArgumentParser(description="Plot SD correlation results from spatialDecompose")
@@ -43,20 +45,41 @@ for i in range(numIonTypes):
       sdCorr2[zipIndexPair2(i, j, numIonTypes)] = (sdCorr[zipIndexPair(i, j, numIonTypes)] +
                                                      sdCorr[zipIndexPair(j, i, numIonTypes)]) / 2
 
+class MidpointNormalize(Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.42, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
+
 nm2AA = 10
 
 fig, axs = plt.subplots(1, numIonTypePairs, sharex=True, sharey=True, figsize=(18, 5))
 tmin, tmax = 0, 70
 rmin, rmax = 25, 120
+vmin, vmax = (np.nanmin(sdCorr2[:, rmin:rmax, tmin:tmax]) * nm2AA**2,
+              np.nanmax(sdCorr2[:, rmin:rmax, tmin:tmax]) * nm2AA**2)
 T, R = np.meshgrid(timeLags[tmin:tmax], rBins[rmin:rmax] * nm2AA)
+#bounds = np.linspace(vmin, vmax, endpoint=True)
+bounds = np.arange(-0.05, 0.301, 0.025)
+#cmap = cm.get_cmap('RdBu_r')
+cmap = cm.get_cmap('RdYlBu_r', 28)
+norm = MidpointNormalize(midpoint=0, vmin=-0.3, vmax=0.3)
+
 for i, (ax, sd) in enumerate(zip(axs.flat, sdCorr2)):
   if (i < numIonTypePairs):
-    c = ax.contourf(T, R, sd[rmin:rmax, tmin:tmax] * nm2AA**2)
+    c = ax.contourf(T, R, sd[rmin:rmax, tmin:tmax] * nm2AA**2, bounds, norm=norm, cmap=cmap)
     ax.set_xlabel(r'$t$  (ps)')
     ax.set_title('{}'.format(i))
     if (i == 0):
       ax.set_ylabel(r'$r$  ($\AA$)')
-cb = fig.colorbar(c, ax=axs.ravel().tolist())
+
+plt.tight_layout()
+cb = plt.colorbar(c, ax=axs.ravel().tolist(), ticks=np.arange(-0.05, 0.301, 0.05))
 cb.set_label(r'$c_{IL}^{(2)}(t;r)$  ($\AA^2$ ps$^{-2}$)')
 
 plt.ion()
