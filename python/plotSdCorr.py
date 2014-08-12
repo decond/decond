b8ff22b628da129b6749fb9a508408d6dffef1d7
KM Tu <tutu522@gmail.com>
2014-08-13 03:12:19 +0900

    Adds RDF threshold filter to plotSdCorr.py

--- python/plotSdCorr.py
+++ python/plotSdCorr.py
@@ -10,7 +10,10 @@ from itertools import accumulate
 parser = argparse.ArgumentParser(description="Plot SD correlation results from spatialDecompose")
 parser.add_argument('sdcorrData', help="SD correlation data file <sdcorr.h5>")
 parser.add_argument('-o', '--out', default='corr', help="output figure base filename, default = 'sdcorr'")
+parser.add_argument('--threshold', type=float, default=0, help="RDF threshold for plotting, default = 0")
 args = parser.parse_args()
+
+threshold = args.threshold
 
 def zipIndexPair(idx_r, idx_c, size):
   """
@@ -32,18 +35,36 @@ with h5py.File(args.sdcorrData, 'r') as f:
   timeLags = f['timeLags'][...]
   rBins = f['rBins'][...]
   sdCorr = f['sdCorr'][...]  # nm^2 / ps^2
+  rho = f['rho'][...]
+  volume = f['volume'][...]
   numMol = f.attrs['numMol'][...]
   numIonTypes = numMol.size
   numIonTypePairs = (numIonTypes*(numIonTypes+1)) // 2;
 
 sdCorr2 = np.empty([numIonTypePairs, rBins.size, timeLags.size])
+rho2 = np.empty([numIonTypes * (numIonTypes + 1) / 2, rBins.size])
 for i in range(numIonTypes):
   for j in range(i, numIonTypes):
     if (i == j):
+      rho2[zipIndexPair2(i, j, numIonTypes)] = rho[zipIndexPair(i, j, numIonTypes)]
       sdCorr2[zipIndexPair2(i, j, numIonTypes)] = sdCorr[zipIndexPair(i, j, numIonTypes)]
     else:
+      rho2[zipIndexPair2(i, j, numIonTypes)] = (rho[zipIndexPair(i, j, numIonTypes)] +
+                                                rho[zipIndexPair(j, i, numIonTypes)]) / 2
       sdCorr2[zipIndexPair2(i, j, numIonTypes)] = (sdCorr[zipIndexPair(i, j, numIonTypes)] +
                                                      sdCorr[zipIndexPair(j, i, numIonTypes)]) / 2
+
+density = numMol / volume
+dr = rBins[1] - rBins[0]
+dv = 4 * np.pi * rBins**2 * dr
+vol = volume
+rho_Vdv = rho2
+rho_V = rho_Vdv / dv
+rho_dv = rho_Vdv / vol
+rho = rho_V / vol
+g = rho / np.array([d1 * d2 for (e1, d1) in enumerate(density)
+                            for (e2, d2) in enumerate(density) if e2 >= e1]
+                  )[:, np.newaxis]
 
 class MidpointNormalize(Normalize):
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
@@ -70,9 +91,16 @@ bounds = np.arange(-0.05, 0.301, 0.025)
 cmap = cm.get_cmap('RdYlBu_r', 28)
 norm = MidpointNormalize(midpoint=0, vmin=-0.3, vmax=0.3)
 
+smallRegion = []
+for rdf in g:
+  smallRegion.append(next(i for i, v in enumerate(rdf) if v >= 1))
+print("smallRegion =", smallRegion)
+
 for i, (ax, sd) in enumerate(zip(axs.flat, sdCorr2)):
   if (i < numIonTypePairs):
-    c = ax.contourf(T, R, sd[rmin:rmax, tmin:tmax] * nm2AA**2, bounds, norm=norm, cmap=cmap)
+    sd_masked = np.ma.masked_where(np.array([[c if j <= smallRegion[i] else False
+                                     for j, c in enumerate(g[i] < threshold)]] * timeLags.size).T, sd)
+    c = ax.contourf(T, R, sd_masked[rmin:rmax, tmin:tmax] * nm2AA**2, bounds, norm=norm, cmap=cmap)
     ax.set_xlabel(r'$t$  (ps)')
     ax.set_title('{}'.format(i))
     if (i == 0):
