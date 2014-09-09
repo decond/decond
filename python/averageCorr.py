ca620664d220e5bf8f1efd148dd7071c7692cdbc
KM Tu <tutu522@gmail.com>
2014-09-10 01:19:27 +0900

    Adds --nosd option to averageCorr.py and plotCorr.py
    auto-detection is also available

--- python/averageCorr.py
+++ python/averageCorr.py
@@ -17,7 +17,18 @@ parser.add_argument('-m', '--memoryFriendly', action='store_true',
 parser.add_argument('-e', '--error', action='store_true',
                     help="calculate and output the errors."
                          "Files are loaded twice if memory-friendly mode is on.")
+parser.add_argument('--nosd', action='store_true', help="no-SD mode, i.e. one-two only mode")
 args = parser.parse_args()
+
+if (not args.nosd):
+  try:
+    for data in args.corrData:
+      with h5py.File(data, 'r') as f:
+        rBins = f['rBins'][...]
+  except KeyError as e:
+    print("Warning: no 'rBins' dataset is found in (at least)", data)
+    print("Automatically change to --nosd mode")
+    args.nosd = True
 
 def zipIndexPair(idx_r, idx_c, size):
   """
@@ -38,9 +49,15 @@ def zipIndexPair2(idx_r, idx_c, size):
 numMD = len(args.corrData)
 if (args.out == None):
   if (args.window == 1):
-    outFilename = 'corr.ave' + str(numMD) + '.h5'
+    if (args.nosd):
+      outFilename = 'corr-nosd.ave' + str(numMD) + '.h5'
+    else:
+      outFilename = 'corr.ave' + str(numMD) + '.h5'
   else:
-    outFilename = 'corr.ave' + str(numMD) + '.w' + str(args.window) + '.h5'
+    if (args.nosd):
+      outFilename = 'corr-nosd.ave' + str(numMD) + '.w' + str(args.window) + '.h5'
+    else:
+      outFilename = 'corr.ave' + str(numMD) + '.w' + str(args.window) + '.h5'
 else:
   outFilename = args.out if args.out.split('.')[-1] == 'h5' else args.out + '.h5'
 
@@ -57,92 +74,103 @@ if (args.memoryFriendly):
         numIonTypes = numMol.size
         numIonTypePairs = (numIonTypes*(numIonTypes+1)) / 2;
         charge = f.attrs['charge']
-        rBinsTmp = f['rBins'][:]
-        rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
-        rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
-                                       axis=1, keepdims=True), axis=0)
         timeLags = f['timeLags'][:]
         nCorr = np.zeros([numIonTypes*(numIonTypes+1), timeLags.size])
-        sdCorr = np.zeros([numIonTypes**2, rBins.size, timeLags.size])
-        rho = np.zeros([numIonTypes**2, rBins.size])
         volume = 0
+        if (not args.nosd):
+          rBinsTmp = f['rBins'][:]
+          rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
+          rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
+                                         axis=1, keepdims=True), axis=0)
+          sdCorr = np.zeros([numIonTypes**2, rBins.size, timeLags.size])
+          rho = np.zeros([numIonTypes**2, rBins.size])
 
       if (f['timeLags'].size != timeLags.size):
         isTimeLagsChanged = True
         if (f['timeLags'].size < timeLags.size):
           timeLags = f[timeLags][...]
           nCorr = nCorr[..., :timeLags.size]
-          sdCorr = sdCorr[..., :timeLags.size]
+          if (not args.nosd):
+            sdCorr = sdCorr[..., :timeLags.size]
 
-      if (f['rBins'].size < rBins.size*args.window):
-        rBinsTmp = f['rBins'][:]
-        rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
-        rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
-                                                axis=1, keepdims=True), axis=0)
-        sdCorr = sdCorr[..., :rBins.size, :]
-        rho = rho[..., :rBins.size]
+      if (not args.nosd):
+        if (f['rBins'].size < rBins.size*args.window):
+          rBinsTmp = f['rBins'][:]
+          rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
+          rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
+                                                  axis=1, keepdims=True), axis=0)
+          sdCorr = sdCorr[..., :rBins.size, :]
+          rho = rho[..., :rBins.size]
 
       nCorr += f['nCorr'][:, :timeLags.size]
-
-      sdCorrTmp = f['sdCorr'][:, :rBins.size*args.window, :timeLags.size] # [type, rBin*window, time]
-      rhoTmp = f['rho'][:, :rBins.size*args.window] # [type, rBin*window]
-      sdCorrTmp = sdCorrTmp * rhoTmp[:, :, np.newaxis] # [type, rBin*window, time]
-      sdCorrTmp = np.array(np.split(sdCorrTmp, rBins.size, axis=1)) # [rBin, type, window, time]
-      sdCorrTmp = np.swapaxes(np.sum(sdCorrTmp, axis=2), 0, 1) # [type, rBin, time]
-      rhoTmp = np.array(np.split(rhoTmp, rBins.size, axis=1)) # [rBin, type, window]
-      rhoTmp = np.sum(rhoTmp, axis=2).T # [type, rBin]
-
-      sdCorr += sdCorrTmp / rhoTmp[:, :, np.newaxis]
-      rho += rhoTmp
 
       if ('cell' in f.attrs.keys()):
         volume += f.attrs['cell'].prod()
       else:
         volume += f['volume'][...]
 
+      if (not args.nosd):
+        sdCorrTmp = f['sdCorr'][:, :rBins.size*args.window, :timeLags.size] # [type, rBin*window, time]
+        rhoTmp = f['rho'][:, :rBins.size*args.window] # [type, rBin*window]
+        sdCorrTmp = sdCorrTmp * rhoTmp[:, :, np.newaxis] # [type, rBin*window, time]
+        sdCorrTmp = np.array(np.split(sdCorrTmp, rBins.size, axis=1)) # [rBin, type, window, time]
+        sdCorrTmp = np.swapaxes(np.sum(sdCorrTmp, axis=2), 0, 1) # [type, rBin, time]
+        rhoTmp = np.array(np.split(rhoTmp, rBins.size, axis=1)) # [rBin, type, window]
+        rhoTmp = np.sum(rhoTmp, axis=2).T # [type, rBin]
+
+        sdCorr += sdCorrTmp / rhoTmp[:, :, np.newaxis]
+        rho += rhoTmp
+
   nCorr /= numMD
-  sdCorr /= numMD
-  rho /= numMD
   volume /= numMD
+  if (not args.nosd):
+    sdCorr /= numMD
+    rho /= numMD
 
   if (args.error):
     print("determine the errors... ")
     if (numMD > 1):
       nCorr_std = np.zeros_like(nCorr)
-      sdCorr_std = np.zeros_like(sdCorr)
-      rho_std = np.zeros_like(rho)
       volume_std = 0.
+      if (not args.nosd):
+        sdCorr_std = np.zeros_like(sdCorr)
+        rho_std = np.zeros_like(rho)
 
       for n, data in enumerate(args.corrData):
         print("reading " + data)
         with h5py.File(data, 'r') as f:
           nCorr_std += (f['nCorr'][:, :timeLags.size] - nCorr)**2
-          sdCorr_std += (f['sdCorr'][:, :rBins.size, :timeLags.size] - sdCorr)**2
-          rho_std += (f['rho'][:, :rBins.size] - rho)**2
           if ('cell' in f.attrs.keys()):
             volume_std += (f.attrs['cell'].prod() - volume)**2
           else:
             volume_std += (f['volume'] - volume)**2
 
-      nCorr_std = np.sqrt(nCorr_std / (numMD - 1)) 
-      sdCorr_std = np.sqrt(sdCorr_std / (numMD - 1)) 
-      rho_std = np.sqrt(rho_std / (numMD - 1))
-      volume_std = np.sqrt(volume_std / (numMD - 1))
+          if (not args.nosd):
+            sdCorr_std += (f['sdCorr'][:, :rBins.size, :timeLags.size] - sdCorr)**2
+            rho_std += (f['rho'][:, :rBins.size] - rho)**2
 
+      nCorr_std = np.sqrt(nCorr_std / (numMD - 1)) 
+      volume_std = np.sqrt(volume_std / (numMD - 1))
       nCorr_err = nCorr_std / np.sqrt(numMD)
-      sdCorr_err = sdCorr_std / np.sqrt(numMD)
-      rho_err = rho_std / np.sqrt(numMD)
       volume_err = volume_std / np.sqrt(numMD)
+
+      if (not args.nosd):
+        sdCorr_std = np.sqrt(sdCorr_std / (numMD - 1)) 
+        rho_std = np.sqrt(rho_std / (numMD - 1))
+        sdCorr_err = sdCorr_std / np.sqrt(numMD)
+        rho_err = rho_std / np.sqrt(numMD)
+
     else:
       # numMD = 1
       nCorr_err = np.empty_like(nCorr)
       nCorr_err.fill(np.nan)
-      sdCorr_err = np.empty_like(sdCorr)
-      sdCorr_err.fill(np.nan)
-      rho_err = np.empty_like(rho)
-      rho_err.fill(np.nan)
       volume_err = np.empty_like(volume)
       volume_err.fill(np.nan)
+      if (not args.nosd):
+        sdCorr_err = np.empty_like(sdCorr)
+        sdCorr_err.fill(np.nan)
+        rho_err = np.empty_like(rho)
+        rho_err.fill(np.nan)
 
 else:
   for n, data in enumerate(args.corrData):
@@ -153,75 +181,81 @@ else:
         numIonTypes = numMol.size
         numIonTypePairs = (numIonTypes*(numIonTypes+1)) / 2;
         charge = f.attrs['charge']
-        rBinsTmp = f['rBins'][:]
-        rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
-        rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
-                                                axis=1, keepdims=True), axis=0)
         timeLags = f['timeLags'][:]
         nCorrN = np.zeros([numMD, numIonTypes*(numIonTypes+1), timeLags.size])
-        sdCorrN = np.zeros([numMD, numIonTypes**2, rBins.size, timeLags.size])
-        rhoN = np.empty([numMD, numIonTypes**2, rBins.size])
         volumeN = np.zeros([numMD])
+        if (not args.nosd):
+          rBinsTmp = f['rBins'][:]
+          rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
+          rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
+                                                axis=1, keepdims=True), axis=0)
+          sdCorrN = np.zeros([numMD, numIonTypes**2, rBins.size, timeLags.size])
+          rhoN = np.empty([numMD, numIonTypes**2, rBins.size])
 
       if (f['timeLags'].size != timeLags.size):
         isTimeLagsChanged = True
         if (f['timeLags'].size < timeLags.size):
           timeLags = f[timeLags][...]
           nCorrN = nCorrN[..., :timeLags.size]
-          sdCorrN = sdCorrN[..., :timeLags.size]
+          if (not args.nosd):
+            sdCorrN = sdCorrN[..., :timeLags.size]
 
-      if (f['rBins'].size < rBins.size*args.window):
-        rBinsTmp = f['rBins'][:]
-        rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
-        rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
-                                                axis=1, keepdims=True), axis=0)
-        sdCorrN = sdCorrN[..., :rBins.size, :]
-        rhoN = rhoN[..., :rBins.size]
+      if (not args.nosd):
+        if (f['rBins'].size < rBins.size*args.window):
+          rBinsTmp = f['rBins'][:]
+          rBinsTmp = rBinsTmp[:rBinsTmp.size // args.window * args.window]
+          rBins = np.concatenate(np.mean(np.split(rBinsTmp, rBinsTmp.size // args.window, axis=0),
+                                                  axis=1, keepdims=True), axis=0)
+          sdCorrN = sdCorrN[..., :rBins.size, :]
+          rhoN = rhoN[..., :rBins.size]
 
       nCorrN[n] = f['nCorr'][:, :timeLags.size]
-
-      sdCorrTmp = f['sdCorr'][:, :rBins.size*args.window, :timeLags.size] # [type, rBin*window, time]
-      rhoTmp = f['rho'][:, :rBins.size*args.window] # [type, rBin*window]
-      sdCorrTmp = sdCorrTmp * rhoTmp[:, :, np.newaxis] # [type, rBin*window, time]
-      sdCorrTmp = np.array(np.split(sdCorrTmp, rBins.size, axis=1)) # [rBin, type, window, time]
-      sdCorrTmp = np.swapaxes(np.sum(sdCorrTmp, axis=2), 0, 1) # [type, rBin, time]
-      rhoTmp = np.array(np.split(rhoTmp, rBins.size, axis=1)) # [rBin, type, window]
-      rhoTmp = np.sum(rhoTmp, axis=2).T # [type, rBin]
-
-      sdCorrN[n] = sdCorrTmp / rhoTmp[:, :, np.newaxis]
-      rhoN[n] = rhoTmp
 
       if ('cell' in f.attrs.keys()):
         volumeN[n] = f.attrs['cell'].prod()
       else:
         volumeN[n] = f['volume'][...]
 
+      if (not args.nosd):
+        sdCorrTmp = f['sdCorr'][:, :rBins.size*args.window, :timeLags.size] # [type, rBin*window, time]
+        rhoTmp = f['rho'][:, :rBins.size*args.window] # [type, rBin*window]
+        sdCorrTmp = sdCorrTmp * rhoTmp[:, :, np.newaxis] # [type, rBin*window, time]
+        sdCorrTmp = np.array(np.split(sdCorrTmp, rBins.size, axis=1)) # [rBin, type, window, time]
+        sdCorrTmp = np.swapaxes(np.sum(sdCorrTmp, axis=2), 0, 1) # [type, rBin, time]
+        rhoTmp = np.array(np.split(rhoTmp, rBins.size, axis=1)) # [rBin, type, window]
+        rhoTmp = np.sum(rhoTmp, axis=2).T # [type, rBin]
+
+        sdCorrN[n] = sdCorrTmp / rhoTmp[:, :, np.newaxis]
+        rhoN[n] = rhoTmp
 
   nCorr = np.mean(nCorrN, axis=0)
-  sdCorr = np.mean(sdCorrN, axis=0)
   volume = np.mean(volumeN, axis=0)
-  rho = np.mean(rhoN, axis=0)
+  if (not args.nosd):
+    rho = np.mean(rhoN, axis=0)
+    sdCorr = np.mean(sdCorrN, axis=0)
 
   if (args.error):
     if (numMD > 1):
       nCorr_std = np.std(nCorrN, axis=0)
       nCorr_err = nCorr_std / np.sqrt(numMD)
-      sdCorr_std = np.std(sdCorrN, axis=0)
-      sdCorr_err = sdCorr_std / np.sqrt(numMD)
       volume_std = np.std(volumeN, axis=0)
       volume_err = volume_std / np.sqrt(numMD)
-      rho_std = np.std(rhoN, axis=0)
-      rho_err = rho_std / np.sqrt(numMD)
+      if (not args.nosd):
+        sdCorr_std = np.std(sdCorrN, axis=0)
+        sdCorr_err = sdCorr_std / np.sqrt(numMD)
+        rho_std = np.std(rhoN, axis=0)
+        rho_err = rho_std / np.sqrt(numMD)
     else:
       # numMD = 1
       nCorr_err = np.empty_like(nCorr)
       nCorr_err.fill(np.nan)
-      sdCorr_err = np.empty_like(sdCorr)
-      sdCorr_err.fill(np.nan)
-      rho_err = np.empty_like(rho)
-      rho_err.fill(np.nan)
       volume_err = np.empty_like(volume)
       volume_err.fill(np.nan)
+      if (not args.nosd):
+        sdCorr_err = np.empty_like(sdCorr)
+        sdCorr_err.fill(np.nan)
+        rho_err = np.empty_like(rho)
+        rho_err.fill(np.nan)
 
 if (isTimeLagsChanged):
   print("Note: the maximum timeLags are different among the corr files\n"
@@ -233,27 +267,28 @@ with h5py.File(args.corrData[0], 'r') as f, h5py.File(outFilename, 'w') as outFi
       outFile.attrs[name] = value
 
   outFile['timeLags'] = timeLags
-  outFile['rBins'] = rBins
   outFile['volume'] = volume
   outFile['nCorr'] = nCorr
-  outFile['sdCorr'] = sdCorr
-  outFile['rho'] = rho
+  outFile['timeLags'].dims.create_scale(outFile['timeLags'], 't')
+  outFile['nCorr'].dims[1].attach_scale(outFile['timeLags'])
   if (args.error):
     outFile['volume_err'] = volume_err
     outFile['nCorr_err'] = nCorr_err
-    outFile['sdCorr_err'] = sdCorr_err
-    outFile['rho_err'] = rho_err
-
-  outFile['timeLags'].dims.create_scale(outFile['timeLags'], 't')
-  outFile['rBins'].dims.create_scale(outFile['rBins'], 'r')
-  outFile['nCorr'].dims[1].attach_scale(outFile['timeLags'])
-  outFile['sdCorr'].dims[1].attach_scale(outFile['rBins'])
-  outFile['sdCorr'].dims[2].attach_scale(outFile['timeLags'])
-  outFile['rho'].dims[1].attach_scale(outFile['rBins'])
-  if (args.error):
     outFile['nCorr_err'].dims[1].attach_scale(outFile['timeLags'])
-    outFile['sdCorr_err'].dims[1].attach_scale(outFile['rBins'])
-    outFile['sdCorr_err'].dims[2].attach_scale(outFile['timeLags'])
-    outFile['rho_err'].dims[1].attach_scale(outFile['rBins'])
+
+  if (not args.nosd):
+    outFile['rBins'] = rBins
+    outFile['sdCorr'] = sdCorr
+    outFile['rho'] = rho
+    outFile['rBins'].dims.create_scale(outFile['rBins'], 'r')
+    outFile['sdCorr'].dims[1].attach_scale(outFile['rBins'])
+    outFile['sdCorr'].dims[2].attach_scale(outFile['timeLags'])
+    outFile['rho'].dims[1].attach_scale(outFile['rBins'])
+    if (args.error):
+      outFile['sdCorr_err'] = sdCorr_err
+      outFile['rho_err'] = rho_err
+      outFile['sdCorr_err'].dims[1].attach_scale(outFile['rBins'])
+      outFile['sdCorr_err'].dims[2].attach_scale(outFile['timeLags'])
+      outFile['rho_err'].dims[1].attach_scale(outFile['rBins'])
 
 print("File is output as: " + outFilename)
--- python/plotCorr.py
+++ python/plotCorr.py
@@ -10,14 +10,32 @@ import itertools as it
 
 parser = argparse.ArgumentParser(description="Plot decomposed correlation")
 parser.add_argument('corrData', help="correlation data file <corr.h5>")
-parser.add_argument('-o', '--out', default='corr', help="output figure base filename, default = 'sdcorr'")
+parser.add_argument('-o', '--out', default='corr', help="output figure base filename, default = 'corr'")
 parser.add_argument('--threshold', type=float, default=0, help="RDF threshold for plotting sdCorr, default = 0")
 parser.add_argument('--color', nargs='*', help="manually assign line color for each auto and cross terms. "
                                     "<auto1>...<autoN> <cross11>...<cross1N> <cross22>...<cross2N> .. <crossNN>")
 parser.add_argument('--label', nargs='*', help="manually assign label for each component. <mol1>...<molN>")
+parser.add_argument('--nosd', action='store_true', help="no-SD mode, i.e. one-two only mode")
 args = parser.parse_args()
 
 threshold = args.threshold
+
+if (not args.nosd):
+  with h5py.File(args.corrData, 'r') as f:
+    try:
+      rBins = f['rBins'][...]
+    except KeyError as e:
+      print("Warning: no 'rBins' dataset is found in", args.corrData)
+      print("Automatically change to --nosd mode")
+      args.nosd = True
+
+if (args.out is None):
+  if (args.nosd):
+    outFilename = 'corr-nosd'
+  else:
+    outFilename = 'corr'
+else:
+  outFilename = args.out if args.out.split('.')[-1] == 'h5' else args.out + '.h5'
 
 class Const:
   """
@@ -43,14 +61,15 @@ def zipIndexPair2(idx_r, idx_c, size):
 
 with h5py.File(args.corrData, 'r') as f:
   timeLags = f['timeLags'][...]
-  rBins = f['rBins'][...]
   nCorr = f['nCorr'][...]  # nm^2 / ps^2
-  sdCorr = f['sdCorr'][...]  # nm^2 / ps^2
-  rho = f['rho'][...]
   volume = f['volume'][...]
   numMol = f.attrs['numMol'][...]
   numIonTypes = numMol.size
   numIonTypePairs = (numIonTypes*(numIonTypes+1)) // 2;
+  if (not args.nosd):
+    rBins = f['rBins'][...]
+    sdCorr = f['sdCorr'][...]  # nm^2 / ps^2
+    rho = f['rho'][...]
 
 # validate arguments
 if (args.color is not None):
@@ -89,91 +108,92 @@ leg = plt.legend()
 plt.xlabel(r't  (ps)')
 plt.ylabel(r'$C_I^{(1)}(t)$, $C_{IL}^{(2)}(t)$  ($\AA^2$ ps$^{-2}$)')
 
-# plot sdCorr
-sdCorr2 = np.empty([numIonTypePairs, rBins.size, timeLags.size])
-rho2 = np.empty([numIonTypes * (numIonTypes + 1) / 2, rBins.size])
-for i in range(numIonTypes):
-  for j in range(i, numIonTypes):
-    if (i == j):
-      rho2[zipIndexPair2(i, j, numIonTypes)] = rho[zipIndexPair(i, j, numIonTypes)]
-      sdCorr2[zipIndexPair2(i, j, numIonTypes)] = sdCorr[zipIndexPair(i, j, numIonTypes)]
-    else:
-      rho2[zipIndexPair2(i, j, numIonTypes)] = (rho[zipIndexPair(i, j, numIonTypes)] +
-                                                rho[zipIndexPair(j, i, numIonTypes)]) / 2
-      sdCorr2[zipIndexPair2(i, j, numIonTypes)] = (sdCorr[zipIndexPair(i, j, numIonTypes)] +
-                                                     sdCorr[zipIndexPair(j, i, numIonTypes)]) / 2
+if (not args.nosd):
+  # plot sdCorr
+  sdCorr2 = np.empty([numIonTypePairs, rBins.size, timeLags.size])
+  rho2 = np.empty([numIonTypes * (numIonTypes + 1) / 2, rBins.size])
+  for i in range(numIonTypes):
+    for j in range(i, numIonTypes):
+      if (i == j):
+        rho2[zipIndexPair2(i, j, numIonTypes)] = rho[zipIndexPair(i, j, numIonTypes)]
+        sdCorr2[zipIndexPair2(i, j, numIonTypes)] = sdCorr[zipIndexPair(i, j, numIonTypes)]
+      else:
+        rho2[zipIndexPair2(i, j, numIonTypes)] = (rho[zipIndexPair(i, j, numIonTypes)] +
+                                                  rho[zipIndexPair(j, i, numIonTypes)]) / 2
+        sdCorr2[zipIndexPair2(i, j, numIonTypes)] = (sdCorr[zipIndexPair(i, j, numIonTypes)] +
+                                                       sdCorr[zipIndexPair(j, i, numIonTypes)]) / 2
 
-density = numMol / volume
-dr = rBins[1] - rBins[0]
-dv = 4 * np.pi * rBins**2 * dr
-vol = volume
-rho_Vdv = rho2
-rho_V = rho_Vdv / dv
-rho_dv = rho_Vdv / vol
-rho = rho_V / vol
-g = rho / np.array([d1 * d2 for (e1, d1) in enumerate(density)
-                            for (e2, d2) in enumerate(density) if e2 >= e1]
-                  )[:, np.newaxis]
+  density = numMol / volume
+  dr = rBins[1] - rBins[0]
+  dv = 4 * np.pi * rBins**2 * dr
+  vol = volume
+  rho_Vdv = rho2
+  rho_V = rho_Vdv / dv
+  rho_dv = rho_Vdv / vol
+  rho = rho_V / vol
+  g = rho / np.array([d1 * d2 for (e1, d1) in enumerate(density)
+                              for (e2, d2) in enumerate(density) if e2 >= e1]
+                    )[:, np.newaxis]
 
-class CustomNormalize(Normalize):
-    def __init__(self, vmin=None, vmax=None, vanchor=None, clip=False, canchor=0.5):
-        self.vanchor = vanchor
-        self.canchor = canchor
-        Normalize.__init__(self, vmin, vmax, clip)
+  class CustomNormalize(Normalize):
+      def __init__(self, vmin=None, vmax=None, vanchor=None, clip=False, canchor=0.5):
+          self.vanchor = vanchor
+          self.canchor = canchor
+          Normalize.__init__(self, vmin, vmax, clip)
 
-    def __call__(self, value, clip=None):
-        # I'm ignoring masked values and all kinds of edge cases to make a
-        # simple example...
-        x, y = [self.vmin, self.vanchor, self.vmax], [0, self.canchor, 1]
-        return np.ma.masked_array(np.interp(value, x, y))
+      def __call__(self, value, clip=None):
+          # I'm ignoring masked values and all kinds of edge cases to make a
+          # simple example...
+          x, y = [self.vmin, self.vanchor, self.vmax], [0, self.canchor, 1]
+          return np.ma.masked_array(np.interp(value, x, y))
 
-smallRegion = []
-for rdf in g:
-  smallRegion.append(next(i for i, v in enumerate(rdf) if v >= 1))
-print("smallRegion =", smallRegion)
+  smallRegion = []
+  for rdf in g:
+    smallRegion.append(next(i for i, v in enumerate(rdf) if v >= 1))
+  print("smallRegion =", smallRegion)
 
-sdCorr2_masked = np.ma.masked_where(np.ones_like(sdCorr2) *
-                    np.array([c if j <= smallRegion[i] else False
-                               for j, c in enumerate(g[i] < threshold)])[np.newaxis, :, np.newaxis], sdCorr2)
+  sdCorr2_masked = np.ma.masked_where(np.ones_like(sdCorr2) *
+                      np.array([c if j <= smallRegion[i] else False
+                                 for j, c in enumerate(g[i] < threshold)])[np.newaxis, :, np.newaxis], sdCorr2)
 
-nm2AA = 10
+  nm2AA = 10
 
-tmin, tmax, tstep = 0, 126, 1
-rmin, rmax, rstep = 25, 100, 1
-T, R = np.meshgrid(timeLags[tmin:tmax:tstep], rBins[rmin:rmax:rstep] * nm2AA)
-cmap = cm.get_cmap('RdYlBu_r')
+  tmin, tmax, tstep = 0, 126, 1
+  rmin, rmax, rstep = 25, 100, 1
+  T, R = np.meshgrid(timeLags[tmin:tmax:tstep], rBins[rmin:rmax:rstep] * nm2AA)
+  cmap = cm.get_cmap('RdYlBu_r')
 
-for i, sd in enumerate(sdCorr2_masked):
-  vmin, vmax = (np.nanmin(sdCorr2_masked[i, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2,
-                np.nanmax(sdCorr2_masked[i, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2)
-  norm = CustomNormalize(vanchor=0, canchor=0.42, vmin=vmin, vmax=vmax)
-  plt.figure()
-  c = plt.contourf(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2,
-                   32, norm=norm, cmap=cmap)
-#  plt.contour(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2, [0], colors='black')
-  ax = plt.gca()
-  ax.set_xlabel(r'$t$  (ps)')
-  ax.set_title(label[numIonTypes + i])
-  cb = plt.colorbar(c)
+  for i, sd in enumerate(sdCorr2_masked):
+    vmin, vmax = (np.nanmin(sdCorr2_masked[i, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2,
+                  np.nanmax(sdCorr2_masked[i, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2)
+    norm = CustomNormalize(vanchor=0, canchor=0.42, vmin=vmin, vmax=vmax)
+    plt.figure()
+    c = plt.contourf(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2,
+                     32, norm=norm, cmap=cmap)
+  #  plt.contour(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2, [0], colors='black')
+    ax = plt.gca()
+    ax.set_xlabel(r'$t$  (ps)')
+    ax.set_title(label[numIonTypes + i])
+    cb = plt.colorbar(c)
+    cb.set_label(r'$c_{IL}^{(2)}(t;r)$  ($\AA^2$ ps$^{-2}$)')
+
+  vmin, vmax = (np.nanmin(sdCorr2_masked[:, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2,
+                np.nanmax(sdCorr2_masked[:, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2)
+  norm = CustomNormalize(vanchor=0, canchor=0.42, vmin=-0.1, vmax=0.3)
+  bounds = np.arange(-0.05, 0.301, 0.0125)
+  fig, axs = plt.subplots(1, numIonTypePairs, sharex=True, sharey=True, figsize=(18, 5))
+  for i, (ax, sd) in enumerate(zip(axs.flat, sdCorr2_masked)):
+    c = ax.contourf(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2,
+                    bounds, norm=norm, cmap=cmap)
+  #  ax.contour(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2, [0], colors='black')
+    ax.set_xlabel(r'$t$  (ps)')
+    ax.set_title(label[numIonTypes + i])
+    if (i == 0):
+      ax.set_ylabel(r'$r$  ($\AA$)')
+
+  plt.tight_layout()
+  cb = plt.colorbar(c, ax=axs.ravel().tolist(), ticks=np.arange(-0.05, 0.301, 0.05))
   cb.set_label(r'$c_{IL}^{(2)}(t;r)$  ($\AA^2$ ps$^{-2}$)')
-
-vmin, vmax = (np.nanmin(sdCorr2_masked[:, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2,
-              np.nanmax(sdCorr2_masked[:, rmin:rmax:rstep, tmin:tmax:tstep]) * nm2AA**2)
-norm = CustomNormalize(vanchor=0, canchor=0.42, vmin=-0.1, vmax=0.3)
-bounds = np.arange(-0.05, 0.301, 0.0125)
-fig, axs = plt.subplots(1, numIonTypePairs, sharex=True, sharey=True, figsize=(18, 5))
-for i, (ax, sd) in enumerate(zip(axs.flat, sdCorr2_masked)):
-  c = ax.contourf(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2,
-                  bounds, norm=norm, cmap=cmap)
-#  ax.contour(T, R, sd[rmin:rmax:rstep, tmin:tmax:tstep] * nm2AA**2, [0], colors='black')
-  ax.set_xlabel(r'$t$  (ps)')
-  ax.set_title(label[numIonTypes + i])
-  if (i == 0):
-    ax.set_ylabel(r'$r$  ($\AA$)')
-
-plt.tight_layout()
-cb = plt.colorbar(c, ax=axs.ravel().tolist(), ticks=np.arange(-0.05, 0.301, 0.05))
-cb.set_label(r'$c_{IL}^{(2)}(t;r)$  ($\AA^2$ ps$^{-2}$)')
 
 plt.ion()
 plt.show()
