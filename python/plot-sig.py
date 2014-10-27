5402e195910174d69f47d5ab11fd0dc3d411b21d
KM Tu <tutu522@gmail.com>
2014-10-27 21:30:59 +0900

    Adds plot-sig.py and plot-sig-NaCl.py for individual sig figures

---
+++ python/plot-sig.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.ticker as ticker
+import itertools as it
+
+rc = {'font': {'size': 36,
+               'family': 'serif',
+               'serif': 'Times'},
+      'text': {'usetex': True},
+      'legend': {'fontsize': 34},
+      'axes': {'labelsize': 36},
+      'xtick': {'labelsize': 36,
+                'major.pad': 10,
+                'major.size': 8,
+                'major.width': 1.5,
+                'minor.size': 4,
+                'minor.width': 1.5},
+      'ytick': {'labelsize': 36,
+                'major.pad': 10,
+                'major.size': 8,
+                'major.width': 1.5,
+                'minor.size': 4,
+                'minor.width': 1.5},
+      'lines': {'linewidth': 3}
+     }
+
+
+
+for key in rc:
+  mpl.rc(key, **rc[key])
+
+labelpad = 10
+spineLineWidth = 1.6
+reflinewidth = 1.5
+
+figsize3 = (10, 8.3)
+format='eps'
+
+numIonTypes = 2
+numIonTypePairs = numIonTypes * (numIonTypes+1) // 2
+lineStyle = ['-', '--', '-', '-.', '--']
+
+label = ['cation', 'anion']
+label += ['-'.join(l) for l in it.combinations_with_replacement(label, 2)]
+
+color = ['k', 'k', 'k', 'k', 'k']
+mpl.rcParams['axes.color_cycle'] = color
+
+class Const:
+  """
+  Defines some constants
+  """
+  kB = 1.3806488E-23 #(J K-1)
+  beta = 1 / (kB * 303) #(J-1)
+  basicCharge = 1.60217646E-19 #(Coulomb)
+  ps = 1.0E-12 #(s)
+  nm = 1.0E-9 #(m)
+  nm2AA = 10
+  nm2cm = 1.0E-7
+  ps2s = 1.0E-12
+  D2AA2_ps = nm2AA**2
+  D2cm2_s = nm2cm**2 / ps2s
+
+fitKey = '400.0-600.0'
+
+w1Data = np.load('g.npz')
+rBins_w1 = w1Data['rBins']
+g_w1 = w1Data['g']
+
+w3Data = np.load('g-D-sigI.w3.npz')
+rBins = w3Data['rBins']
+g = w3Data['g']
+DI = w3Data['DI'].item()
+sdD = w3Data['sdD'].item()
+sigI = w3Data['sigI'].item()
+
+numPlots = 1
+threshold = 0.1
+
+#halfCellIndex_w1 = rBins_w1.size / np.sqrt(3)
+#halfCellLength_w1 = rBins_w1[halfCellIndex_w1]
+
+halfCellIndex = rBins.size / np.sqrt(3)
+halfCellLength = rBins[halfCellIndex]
+
+smallRegion = []
+for rdf in g:
+  smallRegion.append(next(i for i, v in enumerate(rdf) if v >= 1))
+
+fig, axs = plt.subplots(numPlots, 1, sharex=False, figsize=figsize3)
+
+abcPos = (0.03, 0.965)
+xticks = np.arange(0, 21, 5)
+
+## plot rdf
+#axs[0].set_color_cycle(color[numIonTypes:])
+#axs[0].axhline(1, linestyle=':', color='black', linewidth=reflinewidth)
+#for i, rdf in enumerate(g_w1):
+#  axs[0].plot(rBins_w1, rdf, label=label[numIonTypes + i])
+#axs[0].legend(loc='upper right')
+##    axs[0].set_title("Fit {} ps".format(fitKey))
+#axs[0].set_xlabel(r"$r$\ \ (\AA)", labelpad=labelpad)
+#axs[0].set_ylabel(r"$\textsl{\textrm{g}}_{IL}(r)$", labelpad=labelpad)
+#plt.text(abcPos[0], abcPos[1], '(a)', transform=axs[0].transAxes,
+#         horizontalalignment='left', verticalalignment='top')
+
+## plot D
+#axs[1].axhline(0, linestyle=':', color='black', linewidth=reflinewidth)
+#for i, D in enumerate(DI[fitKey]):
+#  axs[1].plot(rBins, np.ones_like(rBins)*D, label=label[i], linestyle=lineStyle[i])
+#
+#sdD[fitKey]
+#for i, D in enumerate(sdD[fitKey]):
+#  g_masked = np.where(np.isnan(g[i]), -1, g[i])
+#  D_masked = np.ma.masked_where([c if j <= smallRegion[i] else False
+#                                 for j, c in enumerate(g_masked < threshold)], D)
+#  axs[1].plot(rBins, D_masked, label=label[numIonTypes + i], linestyle=lineStyle[numIonTypes + i])
+#
+#axs[1].set_xlabel(r"$r$\ \ (\AA)", labelpad=labelpad)
+#axs[1].set_ylabel(r"$D^{(1)}_I$, $D^{(2)}_{IL}(r)$\ \ (\AA$^2$ ps$^{-1}$)", labelpad=labelpad)
+##    axs[1].legend(loc='center right')
+#axs[1].legend(loc=(0.437, 0.18), labelspacing=0.2)
+##    axs[1].set_title("threshold {}".format(threshold))
+#plt.text(abcPos[0], abcPos[1], '(b)', transform=axs[1].transAxes,
+#         horizontalalignment='left', verticalalignment='top')
+
+# plot sig
+for i, sig in enumerate(sigI[fitKey]):
+  axs.plot(rBins, sig, label=label[i], linestyle=lineStyle[i])
+  axs.legend(loc='upper right', handlelength=2.2)
+axs.set_xlabel(r"$\lambda$\ \ (\AA)", labelpad=labelpad)
+axs.set_ylabel(r"$\sigma_I(\lambda)$\ \ (S m$^{-1}$)", labelpad=labelpad)
+#plt.text(abcPos[0], abcPos[1], '(c)', transform=axs[2].transAxes,
+#         horizontalalignment='left', verticalalignment='top')
+
+axs.set_ylim(top=0.75)
+
+for ax in [axs]:
+  ax.set_xticks(xticks)
+  ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(5))
+  ax.set_xlim(xmax=halfCellLength)
+  ax.xaxis.labelpad = 1 
+  ax.yaxis.labelpad = 1 
+#  ax.yaxis.set_label_coords(-0.165, 0.5)
+  for sp in ax.spines.values():
+    sp.set_linewidth(spineLineWidth)
+
+#plt.tight_layout()
+#  plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+#plt.subplots_adjust(hspace=0.25)
+plt.savefig('sig.' + format, bbox_inches="tight")
+
+plt.ion()
