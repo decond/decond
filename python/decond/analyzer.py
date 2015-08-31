Backup commit. Averages decCorr with weights. Bugs to be fixed!

python/decond/analyzer.py
@@ -155,6 +155,7 @@ class CorrFile(h5py.File):
 
         self._shrink_corr_buffer(s_sel)
         new_file._shrink_corr_buffer(n_sel)
+#        assert(np.allclose(self.buffer.timeLags, new_file.buffer.timeLags))
 
         def do_dec(dectype):
             sb_dec = getattr(self.buffer, dectype.value)
@@ -164,6 +165,7 @@ class CorrFile(h5py.File):
 
             self._shrink_dec_buffer(dectype, s_sel, s_sel_dec)
             new_file._shrink_dec_buffer(dectype, n_sel, n_sel_dec)
+#            assert(np.allclose(sb_dec.decBins, nb_dec.decBins))
 
         for type_ in DecType:
             if getattr(self.buffer, type_.value) is not None:
@@ -251,11 +253,11 @@ class DecondFile(CorrFile):
                 num_sample = self.buffer.numSample
 
                 buf.decCorr_m2 = np.zeros_like(buf.decCorr)
-                buf.decCorr_err = _m2_to_err(buf.decCorr_m2, num_sample)
+                buf.decCorr_err = _m2_to_err(buf.decCorr_m2, num_sample)  # nan
 
                 buf.decPairCount_m2 = np.zeros_like(buf.decPairCount)
                 buf.decPairCount_err = _m2_to_err(
-                        buf.decPairCount_m2, num_sample)
+                        buf.decPairCount_m2, num_sample)  # nan
 
                 buf.decDCesaro_m2 = np.zeros_like(buf.decDCesaro)
                 buf.decDCesaro_err = _m2_to_err(buf.decDCesaro_m2, num_sample)
@@ -277,10 +279,12 @@ class DecondFile(CorrFile):
 
             def init_dec_m2(buf):
                 num_sample = self.buffer.numSample
-                buf.decCorr_m2 = _err_to_m2(buf.decCorr_err, num_sample)
-                buf.decPairCount_m2 = _err_to_m2(buf.decPairCount_err,
-                                                 num_sample)
-                buf.decDCesaro_m2 = _err_to_m2(buf.decDCesaro_err, num_sample)
+                buf.decCorr_m2 = _err_to_m2(
+                        buf.decCorr_err, num_sample, buf.decPairCount)
+                buf.decDCesaro_m2 = _err_to_m2(
+                        buf.decDCesaro_err, num_sample, buf.decPairCount)
+                buf.decPairCount_m2 = _err_to_m2(
+                        buf.decPairCount_err, num_sample)
 
             for type_ in DecType:
                 buf = getattr(self.buffer, type_.value)
@@ -317,13 +321,44 @@ class DecondFile(CorrFile):
             setattr(buf, data_name + '_err',
                     _m2_to_err(m2, num_sample))
 
+        def add_weighted_data(data_name, weight_name, new_data, new_weight,
+                              dectype=None):
+            """
+            http://www.wikiwand.com/en/Algorithms_for_calculating_variance#/Weighted_incremental_algorithm
+            """
+            if dectype is None:
+                buf = self.buffer
+            else:
+                buf = getattr(self.buffer, dectype.value)
+
+            old_weight = getattr(buf, weight_name)
+            sum_weight = (self.buffer.numSample - 1) * old_weight
+            mean = getattr(buf, data_name)
+            m2 = getattr(buf, data_name + '_m2')
+
+            temp = new_weight + sum_weight
+            delta = new_data - mean
+            r = delta * new_weight[..., np.newaxis] / temp[..., np.newaxis]
+            mean += r
+            m2 += sum_weight[..., np.newaxis] * delta * r
+            sum_weight = temp
+
+            if np.isscalar(mean):
+                setattr(buf, data_name, mean)
+                setattr(buf, data_name + '_m2', m2)
+
+            setattr(buf, data_name + '_err', _m2_to_err(
+                m2, self.buffer.numSample, sum_weight / self.buffer.numSample))
+
         def add_dec_data(dectype, new_buf):
             buf = getattr(new_buf, dectype.value)
-            add_data('decCorr', buf.decCorr, dectype=dectype)
-            add_data('decPairCount', buf.decPairCount,
-                     dectype=dectype)
-            add_data('decDCesaro', buf.decDCesaro,
-                     dectype=dectype)
+            add_weighted_data('decCorr', 'decPairCount',
+                              buf.decCorr, buf.decPairCount, dectype)
+            add_weighted_data('decDCesaro', 'decPairCount',
+                              buf.decDCesaro, buf.decPairCount, dectype)
+
+            # Note that decPairCount must be updated last
+            add_data('decPairCount', buf.decPairCount, dectype)
 
         for sample in samples[begin:]:
             with CorrFile(sample) as f:
@@ -362,14 +397,12 @@ class DecondFile(CorrFile):
         buf.decDCesaro_m2 = buf.decDCesaro_m2[:, sel_dec, sel]
         buf.decDCesaro_err = buf.decDCesaro_err[:, sel_dec, sel]
 
-    def _intersect_buffer(self, new_file):
-        super()._intersect_buffer(new_file)
-
     def _fit_cesaro(self, fit=None):
         if fit is None:
             if not hasattr(self.buffer, 'fit'):
                 raise Error("No fit ranges have been provided")
         else:
+            fit.sort()
             self.buffer.fit = fit
 
     def _write_buffer(self):
@@ -419,16 +452,38 @@ def h5g_to_dict(group):
     return D
 
 
-def _err_to_m2(err, n):
+def _err_to_m2(err, n, w=None):
+    """
+    err: standard error of the mean
+    n: number of samples
+    w: average weight
+    """
     if n > 1:
-        return np.square(err) * n * (n - 1)
+        if w is None:
+            return np.square(err) * n * (n - 1)
+        else:
+            try:
+                return np.square(err) * w * n * (n - 1)
+            except ValueError:
+                return np.square(err) * w[..., np.newaxis] * n * (n - 1)
     else:
         return np.zeros_like(err)
 
 
-def _m2_to_err(m2, n):
+def _m2_to_err(m2, n, w=None):
+    """
+    m2: sum of squares of differences from the (current) mean
+    n: number of samples
+    w: average weight
+    """
     if n > 1:
-        return np.sqrt(m2 / ((n - 1) * n))
+        if w is None:
+            return np.sqrt(m2 / ((n - 1) * n))
+        else:
+            try:
+                return np.sqrt(m2 / ((n - 1) * n * w))
+            except ValueError:
+                return np.sqrt(m2 / ((n - 1) * n * w[..., np.newaxis]))
     else:
         return np.full(m2.shape, np.nan)
 
@@ -480,3 +535,31 @@ def fit_decond(outname, decname, fit):
             outfile.buffer = infile.buffer
         outfile._fit_cesaro(fit)
         return outfile.buffer
+
+
+def weighted_incremental_variance(dataweightpairs, mean=0, variance=0,
+                                  numsample=0, sumweight=0):
+    """
+    http://www.wikiwand.com/en/Algorithms_for_calculating_variance#/Weighted_incremental_algorithm
+    """
+    if numsample > 1:
+        m2 = variance * (numsample - 1) / numsample * sumweight
+    else:
+        m2 = 0
+
+    for x, weight in dataweightpairs:
+        numsample += 1
+        temp = weight + sumweight
+        delta = x - mean
+        r = delta * weight / temp
+        mean = mean + r
+        m2 = m2 + sumweight * delta * r
+        sumweight = temp
+
+    variance_n = m2 / sumweight
+
+    if numsample > 1:
+        variance = variance_n * numsample / (numsample - 1)
+    else:
+        variance = np.full(m2.shape, np.nan)
+    return mean, variance, numsample, sumweight

python/decond/tests/analyzer_test.py
@@ -3,6 +3,7 @@ from .. import analyzer as da
 from scipy import stats
 import os
 import os.path
+import h5py
 
 
 def test_get_inner_index():
@@ -20,7 +21,7 @@ def test_get_inner_index():
     assert(a[a_sel][0] == b[b_sel][0])
     assert(a[a_sel][-1] == b[b_sel][-1])
 
-    print("_get_inner_index: pass")
+#    print("test_get_inner_index: pass")
 
 
 def rand_c5(filename, nummoltype, timeLags=None, base_timeLags=None,
@@ -48,15 +49,15 @@ def rand_c5(filename, nummoltype, timeLags=None, base_timeLags=None,
             scale = base_timeLags[1] - base_timeLags[0]
 
         timeLags = rand_axis(0, end, scale, begin_fixed=True)
-        timeLags_unit = np.string_('ps')
+    timeLags_unit = np.string_('ps')
 
     if r_decbins is None:
         if base_r_decbins is None:
             end = 8
             scale = 0.01
         else:
-            end = base_timeLags.size
-            scale = base_timeLags[1] - base_timeLags[0]
+            end = base_r_decbins.size
+            scale = base_r_decbins[1] - base_r_decbins[0]
 
         r_decbins = rand_axis(0, end, scale, begin_fixed=True)
 
@@ -66,8 +67,9 @@ def rand_c5(filename, nummoltype, timeLags=None, base_timeLags=None,
             end = 7
             scale = 0.1
         else:
-            end = base_timeLags.size
-            scale = base_timeLags[1] - base_timeLags[0]
+            scale = base_e_decbins[1] - base_e_decbins[0]
+            begin = int(base_e_decbins[0] / scale + 0.5)
+            end = begin + base_timeLags.size - 1
 
         e_decbins = rand_axis(begin, end, scale)
 
@@ -122,7 +124,7 @@ def rand_c5(filename, nummoltype, timeLags=None, base_timeLags=None,
 
     nCorr = (corr_amp *
              np.random.sample((numalltype, numtbin)) -
-             corr_amp / 2 + corr_offset) / numrbin
+             corr_amp / 2 + corr_offset) / numtbin
 
     nCorr_unit = np.string_('nm$^2$ ps$^{-2}$')
 
@@ -131,7 +133,7 @@ def rand_c5(filename, nummoltype, timeLags=None, base_timeLags=None,
     sd_buf.decBins_unit = np.string_('nm')
     sd_buf.decCorr = (corr_amp *
                       np.random.sample((numpairtype, numrbin, numtbin)) -
-                      corr_amp / 2 + corr_offset) / numrbin
+                      corr_amp / 2 + corr_offset) / (numtbin * numrbin)
     sd_buf.decCorr_unit = np.string_('nm$^2$ ps$^{-2}$')
     sd_buf.decPairCount = (paircount_amp *
                            np.random.sample((numpairtype, numrbin)) -
@@ -142,7 +144,7 @@ def rand_c5(filename, nummoltype, timeLags=None, base_timeLags=None,
     ed_buf.decBins_unit = np.string_('kcal mol$^{-1}$')
     ed_buf.decCorr = (corr_amp *
                       np.random.sample((numpairtype, numebin, numtbin)) -
-                      corr_amp / 2 + corr_offset) / numebin
+                      corr_amp / 2 + corr_offset) / (numtbin * numebin)
     ed_buf.decCorr_unit = np.string_('nm$^2$ ps$^{-2}$')
     ed_buf.decPairCount = (paircount_amp *
                            np.random.sample((numpairtype, numebin)) -
@@ -170,10 +172,6 @@ def cal_mean_sem(files):
         for cf2 in cfs[i+1:]:
             cf1._intersect_buffer(cf2)
 
-    for cf in cfs:
-        for dectype in da.DecType:
-            buf = getattr(cf.buffer, dectype.value)
-
     # initialize results buffer
     results = da.CorrFile._Buffer()
     for dectype in da.DecType:
@@ -197,16 +195,58 @@ def cal_mean_sem(files):
             res_buf.decPairCount_N.append(data_buf.decPairCount)
 
     # calculate mean and sem
+    results.timeLags = cfs[0].buffer.timeLags
     results.volume = np.mean(results.volume_N, axis=0)
     results.volume_err = stats.sem(results.volume_N)
     results.nCorr = np.mean(results.nCorr_N, axis=0)
     results.nCorr_err = stats.sem(results.nCorr_N)
     for dectype in da.DecType:
         res_buf = getattr(results, dectype.value)
-        res_buf.decCorr = np.mean(res_buf.decCorr_N, axis=0)
-        res_buf.decCorr_err = stats.sem(res_buf.decCorr_N)
+
+        res_buf.decBins = getattr(cfs[0].buffer, dectype.value).decBins
+        res_buf.decCorr_N = np.array(res_buf.decCorr_N)
+        res_buf.decPairCount_N = np.array(res_buf.decPairCount_N)
+        weights = res_buf.decPairCount_N[..., np.newaxis].repeat(
+                res_buf.decCorr_N.shape[-1], axis=-1)
+        res_buf.decCorr = np.average(
+                res_buf.decCorr_N, axis=0, weights=weights)
+        variance = np.average(
+                (res_buf.decCorr_N - res_buf.decCorr[np.newaxis, ...])**2,
+                weights=weights, axis=0)
+        variance *= len(cfs) / (len(cfs) - 1)
+        res_buf.decCorr_err = np.sqrt(variance / len(cfs))
         res_buf.decPairCount = np.mean(res_buf.decPairCount_N, axis=0)
         res_buf.decPairCount_err = stats.sem(res_buf.decPairCount_N)
+
+        # test weighted_incremental_variance - all at once
+        a = res_buf.decCorr_N
+        b = res_buf.decPairCount_N[..., np.newaxis]
+        res_buf.decCorr2, variance, num_sample, _ = (
+                da.weighted_incremental_variance(zip(a, b)))
+        res_buf.decCorr2_err = np.sqrt(variance / num_sample)
+        assert(np.allclose(np.nan_to_num(res_buf.decCorr),
+               np.nan_to_num(res_buf.decCorr2)))
+        assert(np.allclose(np.nan_to_num(res_buf.decCorr_err),
+               np.nan_to_num(res_buf.decCorr2_err)))
+        # ------------------------------------------------
+
+        # test weighted_incremental_variance - one by one
+        a = res_buf.decCorr_N
+        b = res_buf.decPairCount_N[..., np.newaxis]
+        res_buf.decCorr2, variance, num_sample, sumweight = (
+                da.weighted_incremental_variance([(a[0], b[0])]))
+        for data in zip(a[1:], b[1:]):
+            res_buf.decCorr2, variance, num_sample, sumweight = (
+                    da.weighted_incremental_variance(
+                        [data], res_buf.decCorr2, variance, num_sample,
+                        sumweight))
+
+        res_buf.decCorr2_err = np.sqrt(variance / num_sample)
+        assert(np.allclose(np.nan_to_num(res_buf.decCorr),
+               np.nan_to_num(res_buf.decCorr2)))
+        assert(np.allclose(np.nan_to_num(res_buf.decCorr_err),
+               np.nan_to_num(res_buf.decCorr2_err)))
+        # ------------------------------------------------
 
     for cf in cfs:
         cf.close()
@@ -217,18 +257,27 @@ def cal_mean_sem(files):
 def rand_fit(timeLags):
     dt = timeLags[1] - timeLags[0]
     begin_idx = np.random.randint(np.round(timeLags.size * 0.8))
-    end_idx = np.random.randint(timeLags.size - begin_idx) + begin_idx
+    end_idx = np.random.randint(timeLags.size - begin_idx - 1) + begin_idx + 1
+    assert(end_idx > begin_idx)
     return (begin_idx * dt, end_idx * dt)
 
 
-testfile = ['corr1_test.c5', 'corr2_test.c5', 'corr3_test.c5']
+#testfile = ['corr1_test.c5', 'corr2_test.c5', 'corr3_test.c5']
+testfile = ['corr1_test.c5', 'corr2_test.c5']
 decondtest = 'decond_test.d5'
-nummoltype = np.random.random_integers(2, 5)
+#nummoltype = np.random.random_integers(2, 5)
+nummoltype = 4
 
 
 def test_new_decond():
     for file in testfile:
-        rand_c5(file, nummoltype)
+#        rand_c5(file, nummoltype)
+        rand_c5(file, nummoltype, timeLags=np.arange(0, 10, 1),
+                r_decbins=np.arange(0.05, 1.0, 0.1),
+                e_decbins=np.arange(0, 1.1, 0.1))
+
+#    rand_c5(testfile[0], nummoltype, e_decbins=np.arange(0, 0.8, 0.1))
+#    rand_c5(testfile[1], nummoltype, e_decbins=np.arange(0, 1.2, 0.1))
 
     if os.path.exists(decondtest):
         os.remove(decondtest)
@@ -248,7 +297,14 @@ def test_new_decond():
 
     da.new_decond(decondtest, testfile, fit)
 
+    results_file = 'results.h5'
     results = cal_mean_sem(testfile)
+    with h5py.File(results_file, 'w') as f:
+        for dectype in da.DecType:
+            buf = getattr(results, dectype.value)
+            f[dectype.value + '/decBins'] = getattr(buf, 'decBins')
+            f[dectype.value + '/decCorr'] = getattr(buf, 'decCorr')
+            f[dectype.value + '/decCorr_err'] = getattr(buf, 'decCorr_err')
 
     with da.DecondFile(decondtest) as f:
         buf = f.buffer
@@ -261,15 +317,21 @@ def test_new_decond():
             dec_buf = getattr(buf, dectype.value)
             assert(np.allclose(np.nan_to_num(res_buf.decCorr),
                                np.nan_to_num(dec_buf.decCorr)))
-            assert(np.allclose(np.nan_to_num(res_buf.decCorr_err),
-                               np.nan_to_num(dec_buf.decCorr_err)))
-            res_buf.decPairCount = np.mean(res_buf.decPairCount_N, axis=0)
+            try:
+                assert(np.allclose(np.nan_to_num(res_buf.decCorr_err),
+                                   np.nan_to_num(dec_buf.decCorr_err)))
+            except AssertionError:
+                print(dectype.value)
+                print(buf.timeLags)
+                print(res_buf.decBins)
+                print(dec_buf.decBins)
+                
             assert(np.allclose(np.nan_to_num(res_buf.decPairCount),
                                np.nan_to_num(dec_buf.decPairCount)))
             assert(np.allclose(np.nan_to_num(res_buf.decPairCount_err),
                                np.nan_to_num(dec_buf.decPairCount_err)))
 
-    print("new_decond: pass")
+#    print("test_new_decond: pass")
 
 
 extend_file = ['corr_extend1_test.c5', 'corr_extend2_test.c5']
@@ -285,6 +347,9 @@ def test_extend_decond():
         rand_c5(file, nummoltype)
 
     da.extend_decond(decond_extend[0], decondtest, extend_file)
+
+    with da.DecondFile(decond_extend[0]) as f:
+        assert(f.buffer.numSample == len(testfile) + len(extend_file))
 
     # get common timeLags
     fs = ([da.CorrFile(file) for file in extend_file] +