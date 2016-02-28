3d9b1c59f461ddd16661635daa1ad3179418b2d2
KM Tu <tutu522@gmail.com>
2016-02-28 14:27:28 +0900

    Removes the comments in decond.f90 that should have been cleaned up

--- fortran/src/decond.f90
+++ fortran/src/decond.f90
@@ -1,25 +1,5 @@
 program decond
   use mpiproc
-  !use top, only: system
-  !use correlation, only: corr
-  !use spatial_dec, only: rbinwidth, pos_r, pos_c, sdpaircount, &
-                         !sdcorr, pos, num_rbin, com_pos, sd_binIndex, &
-                         !sd_prep_corrmemory, sd_getbinindex, &
-                         !sd_cal_num_rbin, sd_broadcastpos, sd_prepPosMemory, &
-                         !sd_collectcorr, sd_average, sd_make_rbins, sd_finish
-  !use energy_dec, only: engfiles, ed_readEng, ed_getbinindex, &
-                        !num_ebin, ed_binIndex, ed_prep_corrmemory, &
-                        !skipEng, ed_collectcorr, ed_average, &
-                        !edpaircount, edcorr, ebinwidth, &
-                        !ed_make_ebins, ed_finish, num_engfiles, &
-                        !ed_prep_engfiles
-  !use varpars, only: dp, decond_version, &
-                     !totnummol, sysnumatom, &
-                     !corrfile, datafile, &
-                     !line_len_str, numframe, maxlag, nummoltype, &
-                     !skiptrr, num_moltypepair_all, charge, framecount, &
-                     !cell, timestep, temperature, &
-                     !is_sd, is_ed, sys
   use manager, only: is_pa_mode, is_pm_mode, init_config, read_config, &
                      prepare, decompose, output, finish
   implicit none
