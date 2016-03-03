84279b17008f58b585fb3da6b52af6c328b05772
KM Tu <tutu522@gmail.com>
2016-03-03 20:41:54 +0900

    Automatically `git add` in bump.h

--- bump.sh
+++ bump.sh
@@ -12,7 +12,9 @@ version="$1"
 pyversion="python/decond/_version.py"
 sed -i "/__version__/ s/\".*\"/\"$version\"/" $pyversion
 sed -n '/__version__/ p' $pyversion
+git add $pyversion
 
 ftversion="fortran/src/varpars.f90"
 sed -i "/decond_version/ s/\".*\"/\"$version\"/" $ftversion
 sed -n '/decond_version/ p' $ftversion
+git add $ftversion
