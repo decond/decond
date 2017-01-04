00954e4ac848234db7f6702b07a348d39e9ba96b
KM Tu <tutu522@gmail.com>
2017-01-04 17:55:26 +0900

    Add vsc1 mode to process output from LAMMPS

--- fortran/src/varpars.F90
+++ fortran/src/varpars.F90
@@ -11,9 +11,12 @@ module varpars
   character(len=*), parameter :: dec_mode_ec0 = "ec0"
   character(len=*), parameter :: dec_mode_ec1 = "ec1"
   character(len=*), parameter :: dec_mode_vsc = "vsc"
+  character(len=*), parameter :: dec_mode_vsc0 = "vsc0"  !vsc0 is equal to vsc for backward compatibility
+  character(len=*), parameter :: dec_mode_vsc1 = "vsc1"
   character(len=*), parameter :: dec_mode_vel = "vel"
   character(len=*), parameter :: trj_trr = "trr"
   character(len=*), parameter :: trj_xyz = "xyz"
+  character(len=*), parameter :: trj_lmp = "lmp"
   integer, parameter :: world_dim = 3
   integer :: totnummol, sysnumatom
   character(len=line_len) :: corrfile, trjfile, dec_mode
